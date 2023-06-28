da7702933375d4a893b64ee9bf5eb3292670a27c
jbettmann <jordan.bettmann@gmail.com>
2023-06-28 17:00:36 -0600

    added redirects for localStorage brewery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
-"use client";
-import Image from "next/image";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
-import { signIn, signOut, useSession, getSession } from "next-auth/react";
-import Layout from "./layout";
+import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
-import { redirect, usePathname } from "next/navigation";
-import getUser from "@/lib/getUser";
-import SingleBreweryPage from "./breweries/[breweryId]/page";
 
 export default function Home() {
   const { data: session, status, update } = useSession();
 
-  console.log(session?.user);
+  const savedBreweryId = localStorage.getItem("selectedBreweryId");
+
+  if (session?.user && savedBreweryId) {
+    redirect(`/breweries/${savedBreweryId}`);
+  }
+
+  if (session?.user && !savedBreweryId) {
+    redirect(`/breweries/${session.user.breweries[0]}`);
+  }
+
+  console.log(session);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {session?.user.breweries ? (
