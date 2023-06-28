ac9e3dd69065278429bbd8d6646dea3e598a6cb7
jbettmann <jordan.bettmann@gmail.com>
2023-06-28 17:01:34 -0600

    set localStorage to selected brewery

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { Brewery } from "@/app/types/brewery";
-
-import { signIn, signOut, useSession, getSession } from "next-auth/react";
-import React from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import React, { useEffect, useState } from "react";
 import {
   Accordion,
   AccordionContent,
@@ -11,12 +10,42 @@ import {
 } from "./ui/accordion";
 import Link from "next/link";
 
-const NavBar = async ({ breweries }: { breweries: Brewery[] }) => {
-  // const { data: session, status, update } = useSession();
-  // const session = await getServerSession(authOptions);
+const NavBar = ({ breweries: initialBreweries }: { breweries: Brewery[] }) => {
+  const [selectedBrewery, setSelectedBrewery] = useState<Brewery | null>(null);
+  const [breweries, setBreweries] = useState(initialBreweries);
+
+  // On component mount, check if there's a brewery ID in local storage
+  useEffect(() => {
+    const savedBreweryId = localStorage.getItem("selectedBreweryId");
+    if (savedBreweryId) {
+      const savedBrewery = breweries.find((b) => b._id === savedBreweryId);
+      if (savedBrewery) {
+        setSelectedBrewery(savedBrewery);
+      }
+    }
+  }, [breweries]);
+
+  const handleBreweryClick = (brewery: Brewery) => {
+    // Save the clicked brewery's ID in local storage
+    localStorage.setItem("selectedBreweryId", brewery._id);
+
+    // Update the selected brewery
+    setSelectedBrewery(brewery);
+
+    // Move selected brewery to the beginning of the array and remove duplicates
+    const updatedBreweries = breweries.filter((b) => b._id !== brewery._id);
+    updatedBreweries.unshift(brewery);
+
+    // Ensuring uniqueness in breweries array
+    const uniqueBreweries = updatedBreweries.filter(
+      (v, i, a) => a.findIndex((t) => t._id === v._id) === i
+    );
+
+    setBreweries(uniqueBreweries);
+  };
 
   return (
-    <div className="flex flex-row justify-between">
+    <div className="flex flex-row-reverse justify-between">
       <div>
         {breweries ? (
           <button onClick={() => signOut()}>Sign Out</button>
@@ -24,6 +53,7 @@ const NavBar = async ({ breweries }: { breweries: Brewery[] }) => {
           <button onClick={() => signIn()}>Sign In</button>
         )}
       </div>
+      <Link href={"/"}>Home</Link>
       <Accordion type="single" collapsible>
         <AccordionItem value="item-1">
           <AccordionTrigger>
@@ -32,7 +62,11 @@ const NavBar = async ({ breweries }: { breweries: Brewery[] }) => {
           <AccordionContent>
             <div>
               {breweries.map((brewery: Brewery) => (
-                <Link key={brewery._id} href={`/breweries/${brewery._id}`}>
+                <Link
+                  onClick={() => handleBreweryClick(brewery)}
+                  key={brewery._id}
+                  href={`/breweries/${brewery._id}`}
+                >
                   <h3>{brewery.companyName}</h3>
                 </Link>
               ))}
