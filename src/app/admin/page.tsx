2861e3256c1f56f8709fa54ed3748940f07dbed5
jbettmann <jordan.bettmann@gmail.com>
2023-07-05 16:14:28 -0600

    image database set up

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,48 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { supabase } from "@/lib/supabase";
 
 type Props = {};
 
-const adminPage = (props: Props) => {
-  return <div>Admin Only Allowed</div>;
+const AdminPage: React.FC<Props> = (props: Props) => {
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!file) {
+      console.error("No file selected");
+      alert("No file selected");
+      return;
+    }
+
+    // upload image
+    const filename = `${uuidv4()}-${file.name}`;
+
+    const { data, error } = await supabase.storage
+      .from("Images")
+      .upload(filename, file, {
+        cacheControl: "3600",
+        upsert: false,
+      });
+
+    console.log(data, error);
+    // const filepath = data.path;
+    // save filepath in database
+  };
+
+  const handleFileSelected = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files?.length) {
+      setFile(e.target.files[0]);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="file" name="image" onChange={handleFileSelected} />
+      <button type="submit">Upload image</button>
+    </form>
+  );
 };
 
-export default adminPage;
+export default AdminPage;
