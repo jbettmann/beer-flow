601c1905e54bf3501f37a4e52cb1f784ac8fc6e5
jbettmann <jordan.bettmann@gmail.com>
2023-07-10 13:51:51 -0600

    added NewBrewery type

--- src/app/types/brewery.ts
+++ src/app/types/brewery.ts
@@ -3,8 +3,14 @@ import { Category } from "./category";
 export type Brewery = {
   _id: string;
   companyName: string;
+  image: string;
   owner: number;
   admin: string[];
   staff: string[];
   categories: Category[];
+};
+
+export type NewBrewery = {
+  companyName: string;
+  image: string | undefined;
 };
