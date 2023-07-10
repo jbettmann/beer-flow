1c14547ce1dfcc8255d82a54da77dd4e9bc7bbf8
jbettmann <jordan.bettmann@gmail.com>
2023-07-10 13:50:12 -0600

    Create saveImage.tsx

---
+++ src/lib/saveImage.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import { supabase } from "@/lib/supabase";
+type Props = {
+  file: File | null;
+};
+
+const saveImage = async ({ file }: Props) => {
+  if (!file) {
+    console.error("No file selected");
+    alert("No file selected");
+    return;
+  }
+
+  // upload image
+  const filename = `${uuidv4()}-${file.name}`;
+
+  const { data, error } = await supabase.storage
+    .from("Images")
+    .upload(filename, file, {
+      cacheControl: "3600",
+      upsert: false,
+    });
+
+  const filepath = data?.path;
+
+  return filepath;
+};
+
+export default saveImage;
