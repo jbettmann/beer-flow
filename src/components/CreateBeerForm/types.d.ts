18cede542a2e7136ad60885395a87c16a1e39438
jbettmann <jordan.bettmann@gmail.com>
2023-07-13 17:01:26 -0600

    updated types

--- src/components/CreateBeerForm/types.d.ts
+++ src/components/CreateBeerForm/types.d.ts
@@ -1,25 +1,29 @@
+import { Category, NewCategory } from "@/app/types/category";
+
 interface FormValues {
   name: string;
-  abv: string;
+  abv: number;
+  ibu: number;
   style: string;
   malt: Tag[];
   hops: Tag[];
   description: string;
-  category: string[] | { label: string; value: string }[];
+  category: Category[] | { value: string; label: string }[];
   nameSake: string;
   notes: string;
   image: File | null | string;
-  releaseDate: Date | string | null;
+  releasedOn: Date | string | null;
+  archived: boolean;
 }
 
 interface ErrorValues {
   name?: string;
   abv?: string;
+  ibu?: string;
   style?: string;
   malt?: string;
   hops?: string;
   description?: string;
-
   category?: string;
   nameSake?: string;
   notes?: string;
