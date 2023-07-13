df84ed59a48760c76f197df68200eefef4afe5d9
jbettmann <jordan.bettmann@gmail.com>
2023-07-13 17:01:07 -0600

    Create createCategory.tsx

---
+++ src/lib/createCategory.tsx
@@ -0,0 +1,43 @@
+import { Brewery, NewBrewery } from "@/app/types/brewery";
+import { Category, NewCategory } from "@/app/types/category";
+
+type pageProps = {
+  newCategory: NewCategory;
+  breweryId: string;
+  accessToken: string | undefined;
+};
+
+export default async function createCategory({
+  breweryId,
+  newCategory,
+  accessToken,
+}: pageProps) {
+  if (accessToken) {
+    try {
+      const response = await fetch(
+        `https://beer-bible-api.vercel.app/breweries/${breweryId}/categories`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+          body: JSON.stringify(newCategory),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      const responseData: Category = await response.json();
+      console.log(responseData);
+      return responseData;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  } else {
+    throw new Error("User session not found.");
+  }
+}
