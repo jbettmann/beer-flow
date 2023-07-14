4659fb6b4548c0d51df6b043ba169c7a1da371f2
jbettmann <jordan.bettmann@gmail.com>
2023-07-14 16:18:06 -0600

    added supabase config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "npaxzbaiagyllapbxeps.supabase.co",
+        port: "",
+        pathname: "/storage/**",
+      },
+    ],
+  },
+};
 
 module.exports = {
   ...nextConfig,
