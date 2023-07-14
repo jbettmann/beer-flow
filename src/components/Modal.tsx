b6d84daaf2eb913bb1faabacc9deb5fcf41899e6
jbettmann <jordan.bettmann@gmail.com>
2023-07-14 16:20:39 -0600

    styling

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -42,12 +42,12 @@ export default function Modal({
   return (
     <div
       ref={overlay}
-      className="fixed z-10 left-0 right-0 top-0 bottom-0 mx-auto bg-black/60"
+      className="fixed z-10 left-0 right-0 top-0 bottom-0 mx-auto bg-black/60  overflow-y-scroll"
       onClick={onClick}
     >
       <div
         ref={wrapper}
-        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full sm:w-10/12 md:w-8/12 lg:w-1/2 p-6"
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full sm:w-10/12 md:w-8/12 lg:w-1/2 p-6 h-5/6"
       >
         <button className="btn glass close-btn " onClick={onDismiss}>
           {/* X Close Button SVG */}
