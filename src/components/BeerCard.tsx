dd767d54adb7bdce3ff8c829a37247b2e26aaf7e
jbettmann <jordan.bettmann@gmail.com>
2023-07-14 16:18:51 -0600

    Added ImageDisplay

--- src/components/BeerCard.tsx
+++ src/components/BeerCard.tsx
@@ -1,5 +1,6 @@
 import { Beer } from "@/app/types/beer";
 import React from "react";
+import ImageDisplay from "./ImageDisplay/ImageDisplay";
 
 type Props = {
   beer: Beer;
@@ -8,24 +9,22 @@ type Props = {
 const BeerCard = ({ beer }: Props) => {
   return (
     <div className="bg-slate-100 p-5 rounded-lg">
-      <h1>{beer?.name}</h1>
-      <p>ABV {beer?.abv}%</p>
-      <p>Style: {beer?.style}</p>
+      <div className="flex items-center justify-between">
+        <h1>{beer?.name}</h1>
+        {beer.image && (
+          <ImageDisplay className="beer-card__image " item={beer} />
+        )}
+      </div>
 
-      <p>
-        Malt:{" "}
-        {beer?.malt.map((malt, i) => (
-          <span key={i}>{malt} </span>
-        ))}
-      </p>
+      <p>Style: {beer?.style}</p>
+      <p>ABV {beer?.abv}%</p>
+      {beer?.ibu && <p>IBUs {beer?.ibu}</p>}
+      <p>Malt: {beer?.malt.map((malt, i) => <span key={i}>{malt} </span>)}</p>
       <p>Hops: {beer?.hops.map((hop) => hop).join(", ")}</p>
       <p>Description: {beer?.flavorNotes}</p>
       <p>Aroma: {beer?.aroma}</p>
       <p>
-        Category:{" "}
-        {beer?.category.map((c, i) => (
-          <span key={i}>{c.name}</span>
-        ))}
+        Category: {beer?.category.map((c, i) => <span key={i}>{c.name}</span>)}
       </p>
       <p>Name Details: {beer?.nameSake}</p>
       <p>Other Notes: {beer?.notes}</p>
