65c2d4e4cb65a88b8cec7e2247fa92d3a7029a71
jbettmann <jordan.bettmann@gmail.com>
2023-07-21 14:13:09 -0600

    updated UpdateBeerForm

--- src/components/BeerCard.tsx
+++ src/components/BeerCard.tsx
@@ -18,32 +18,29 @@ type Props = {
 
 const BeerCard = ({ brewery, beerId }: Props) => {
   const [isEditing, setIsEditing] = useState(false);
-  const { data: session } = useSession();
-  const {
-    data: allBeers,
-    error: beersError,
-    mutate,
-  } = useSWR(
-    [
-      `https://beer-bible-api.vercel.app/breweries/${brewery._id}/beers`,
-      session?.user.accessToken,
-    ],
-    getBreweryBeers
-  );
+
+  const { selectedBeers } = useBreweryContext();
+
   const [beer, setBeer] = useState<Beer | undefined>(undefined);
 
+  console.log({ brewery, beerId, selectedBeers });
   useEffect(() => {
-    setBeer(allBeers?.find((beer) => beer._id === beerId));
-  }, [allBeers]);
+    setBeer(selectedBeers?.find((beer) => beer._id === beerId));
+  }, [selectedBeers]);
 
   return (
     beer && (
       <div className="bg-slate-100 p-5 rounded-lg">
-        <button onClick={() => setIsEditing(!isEditing)}>
-          {isEditing ? "Save" : "Edit"}
+        <button className={`btn`} onClick={() => setIsEditing(!isEditing)}>
+          {isEditing ? "Cancel" : "Edit"}
         </button>
         {isEditing ? (
-          <UpdateBeerForm beer={beer} brewery={brewery}  />
+          <UpdateBeerForm
+            beer={beer}
+            brewery={brewery}
+            setIsEditing={setIsEditing}
+            setBeer={setBeer}
+          />
         ) : (
           <>
             <div className="flex items-center justify-between">
@@ -62,14 +59,14 @@ const BeerCard = ({ brewery, beerId }: Props) => {
             <p>ABV {beer?.abv}%</p>
             {beer?.ibu && <p>IBUs {beer?.ibu}</p>}
             <p>
-              Malt: {beer?.malt.map((malt, i) => <span key={i}>{malt} </span>)}
+              Malt: {beer?.malt?.map((malt, i) => <span key={i}>{malt} </span>)}
             </p>
-            <p>Hops: {beer?.hops.map((hop) => hop).join(", ")}</p>
+            <p>Hops: {beer?.hops?.map((hop) => hop).join(", ")}</p>
             <p>Description: {beer?.description}</p>
             {/* <p>Aroma: {beer?.aroma}</p> */}
             <p>
               Category:{" "}
-              {beer?.category.map((c, i) => <span key={i}>{c.name}</span>)}
+              {beer?.category?.map((c, i) => <span key={i}>{c.name}</span>)}
             </p>
             <p>Name Details: {beer?.nameSake}</p>
             <p>Other Notes: {beer?.notes}</p>
