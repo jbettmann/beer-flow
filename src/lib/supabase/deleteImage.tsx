1f69627972850d2e964f5ac8307d819653b4054a
jbettmann <jordan.bettmann@gmail.com>
2023-07-21 14:14:35 -0600

    Create deleteImage.tsx

---
+++ src/lib/supabase/deleteImage.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import { supabase } from "@/lib/supabase";
+import saveImage from "./saveImage";
+type Props = {
+  file: File | null;
+};
+
+// Update Image file in Supabase Storage
+export const deleteImage = async (image: string) => {
+  if (!image) {
+    return;
+  }
+
+  const { data, error } = await supabase.storage.from("Images").remove([image]);
+
+  if (error) {
+    console.error("Error removing previous image: ", error);
+    return;
+  }
+};
