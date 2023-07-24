caba206a4cdc3d753a5bd6dbb9c6e114eae8b40c
jbettmann <jordan.bettmann@gmail.com>
2023-07-24 14:22:44 -0600

    Create page.tsx

---
+++ src/app/@modal/(.)breweries/[breweryId]/categories/[categoryId]/page.tsx
@@ -0,0 +1,20 @@
+import Modal from "@/components/Modal";
+import UpdateCategory from "@/components/UpdateCategory/UpdateCategory";
+import React from "react";
+
+type pageProps = {
+  params: {
+    breweryId: string;
+    categoryId: string;
+  };
+};
+
+const EditCategories = ({ params: { breweryId, categoryId } }: pageProps) => {
+  return (
+    <Modal closeButtonOnly={false}>
+      <UpdateCategory breweryId={breweryId} categoryId={categoryId} />
+    </Modal>
+  );
+};
+
+export default EditCategories;
