a4ef7463a7f3cbaf29cecf5b36f86caa7b7b775f
jbettmann <jordan.bettmann@gmail.com>
2023-07-27 21:57:59 -0600

    remove category that has no beers under it

--- src/components/BreweryProfiles.tsx
+++ src/components/BreweryProfiles.tsx
@@ -5,7 +5,7 @@ import { useBreweryContext } from "@/context/brewery-beer";
 import getBreweryBeers from "@/lib/getBreweryBeers";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
-import { Suspense, useEffect, useState } from "react";
+import { Suspense, useEffect, useMemo, useState } from "react";
 import useSWR from "swr";
 import BeerCategory from "./BeerCategory";
 import getSingleBrewery from "@/lib/getSingleBrewery";
@@ -17,6 +17,7 @@ type pageProps = {
 
 export default function BreweryProfiles({ breweryId }: pageProps) {
   const { data: session } = useSession();
+
   const { data: beers, error: beersError } = useSWR(
     [
       `https://beer-bible-api.vercel.app/breweries/${breweryId}/beers`,
@@ -55,7 +56,12 @@ export default function BreweryProfiles({ breweryId }: pageProps) {
     setCategories(brewery?.categories);
   }, [beers, brewery]);
 
-  console.log({ brewery, beers });
+  // watch for change in selected brewery and beer to update categories
+  useEffect(() => {
+    setCategories(selectedBrewery?.categories || []);
+  }, [selectedBrewery, selectedBeers]);
+
+  console.log({ brewery, beers, session });
   useEffect(() => {
     if (typeof window !== "undefined") {
       const storedOpenCategory = sessionStorage.getItem("openCategory");
@@ -65,7 +71,7 @@ export default function BreweryProfiles({ breweryId }: pageProps) {
     if (typeof window !== "undefined" && selectedBrewery) {
       localStorage.setItem("selectedBreweryId", selectedBrewery._id);
     }
-  }, []);
+  }, [selectedBrewery?._id]);
 
   // Handle category change
   const handleCategoryClick = (categoryKey: any) => {
@@ -80,6 +86,17 @@ export default function BreweryProfiles({ breweryId }: pageProps) {
     }
   };
 
+  const beersForCategory = useMemo(() => {
+    return categories?.map((category, i) => {
+      return selectedBeers?.filter((beer) =>
+        beer.category
+          ? beer.category.some((cat) => cat.name === category.name) &&
+            !beer.archived
+          : false
+      );
+    });
+  }, [selectedBeers, selectedBrewery, categories]);
+
   return (
     <section className="w-1/2 m-auto">
       <h1>{selectedBrewery?.companyName}</h1>
@@ -90,18 +107,20 @@ export default function BreweryProfiles({ breweryId }: pageProps) {
             <span className="loading loading-spinner loading-lg"></span>
           }
         >
-          {brewery?.categories?.length > 0 &&
-            categories?.map((category, i) => (
-              <BeerCategory
-                key={i}
-                category={category}
-                beers={selectedBeers}
-                onClick={() => handleCategoryClick(i)}
-                isOpen={openCategory == i}
-                breweryId={selectedBrewery?._id}
-                setSelectedBrewery={setSelectedBrewery}
-              />
-            ))}
+          {beersForCategory &&
+            beersForCategory.map((beers, i) => {
+              return beers.length > 0 ? (
+                <BeerCategory
+                  key={i}
+                  category={categories[i]}
+                  beers={beers}
+                  onClick={() => handleCategoryClick(i)}
+                  isOpen={openCategory == i}
+                  breweryId={selectedBrewery?._id}
+                  setSelectedBrewery={setSelectedBrewery}
+                />
+              ) : null;
+            })}
           <div className="mt-10">
             <BeerCategory
               key="archived"
