029b203a8e6f1a732d77efb54fca362f02834281
jbettmann <jordan.bettmann@gmail.com>
2023-07-27 21:57:38 -0600

    help filter under archived beers

--- src/components/BeerCategory.tsx
+++ src/components/BeerCategory.tsx
@@ -5,13 +5,14 @@ import Link from "next/link";
 import ImageDisplay from "./ImageDisplay/ImageDisplay";
 import { handleBeerView, isNew } from "@/lib/utils";
 import OptionsButton from "./Buttons/OptionsButton";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { handleDeleteCategory } from "@/lib/handleSubmit/handleDeleteCategory";
 import { useBreweryContext } from "@/context/brewery-beer";
 import { useSession } from "next-auth/react";
 import { mutate } from "swr";
 import { set } from "mongoose";
 import { Brewery } from "@/app/types/brewery";
+import { on } from "events";
 
 type Props = {
   category: Category;
@@ -34,11 +35,11 @@ export default function BeerCategory({
   // State for managing the visibility of the options container
   const [isOptionsOpen, setIsOptionsOpen] = useState(false);
   const { selectedBeers, selectedBrewery } = useBreweryContext();
-
+  // manage archived cat arrow state
+  const [isArchivedOpen, setIsArchivedOpen] = useState<string | null>(null);
   const { data: session } = useSession();
 
-  console.log({ selectedBrewery, selectedBeers });
-  const filteredBeers = () => {
+  const filteredBeers = useMemo(() => {
     if (!beers) return [];
     if (category.name === "All Beers") {
       return beers;
@@ -48,22 +49,25 @@ export default function BeerCategory({
     }
     return beers.filter((beer) =>
       beer.category
-        ? beer.category.some((cat) => cat.name === category.name)
+        ? beer.category.some((cat) => cat.name === category.name) &&
+          !beer.archived
         : false
     );
-  };
+  }, [beers, category.name]);
 
   // This assumes each beer belongs to exactly one category.
   const groupBeersByCategory = (beers: Beer[]) => {
     const groupedBeers: { [key: string]: Beer[] } = {};
 
     for (let beer of beers) {
-      const categoryName = beer.category[0].name;
-
-      if (groupedBeers[categoryName]) {
-        groupedBeers[categoryName].push(beer);
-      } else {
-        groupedBeers[categoryName] = [beer];
+      for (let category of beer.category) {
+        if (category.name !== "Archived") {
+          if (groupedBeers[category.name]) {
+            groupedBeers[category.name].push(beer);
+          } else {
+            groupedBeers[category.name] = [beer];
+          }
+        }
       }
     }
 
@@ -105,10 +109,50 @@ export default function BeerCategory({
   const renderArchivedBeers = () => {
     if (!beers) return null;
     const archivedBeers = beers.filter((beer) => beer.archived);
-    return renderAllBeers(archivedBeers);
+    const groupedBeers = groupBeersByCategory(archivedBeers);
+
+    return Object.entries(groupedBeers).map(
+      ([categoryName, beersInCategory]) => {
+        const isOpen = categoryName === isArchivedOpen;
+
+        return (
+          <div
+            key={categoryName}
+            className={`collapse ${isOpen ? "collapse-open" : ""} bg-base-200 `}
+            onClick={(e) => {
+              setIsArchivedOpen(isOpen ? null : categoryName),
+                e.stopPropagation();
+            }}
+          >
+            <div className="collapse-title text-xl font-medium">
+              <p>{categoryName}</p>
+            </div>
+            <div className="collapse-content pb-0">
+              {beersInCategory.map((beer) => (
+                <Link
+                  className="flex items-center justify-between"
+                  href={`/breweries/${breweryId}/beers/${beer._id}`}
+                  key={beer._id}
+                >
+                  <div className="inline-flex items-center">
+                    {beer.image && (
+                      <ImageDisplay
+                        className="beer-category__image"
+                        item={beer}
+                      />
+                    )}
+                    {beer.name}
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
+        );
+      }
+    );
   };
 
-  const beersInCategory = filteredBeers();
+  const beersInCategory = filteredBeers;
 
   // Check if any beer in the category is new
   const isCategoryNew = beersInCategory.some((beer) => isNew(beer));
@@ -158,7 +202,7 @@ export default function BeerCategory({
           <div className="flex flex-col">
             {category.name === "Archived"
               ? renderArchivedBeers()
-              : filteredBeers().map((beer) => (
+              : filteredBeers.map((beer) => (
                   <Link
                     className="flex items-center justify-between"
                     href={`/breweries/${breweryId}/beers/${beer._id}`}
@@ -184,11 +228,13 @@ export default function BeerCategory({
         </div>
       </div>
 
-      <OptionsButton
-        handleOptions={handleOptions}
-        className="btn btn-circle btn-ghost"
-        options={options}
-      />
+      {category.name !== "Archived" && (
+        <OptionsButton
+          handleOptions={handleOptions}
+          className="btn btn-circle btn-ghost"
+          options={options}
+        />
+      )}
     </div>
   );
 }
