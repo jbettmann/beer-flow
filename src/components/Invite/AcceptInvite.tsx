39e36dbbf6150854128ecadedb8f87a0bcc69822
jbettmann <jordan.bettmann@gmail.com>
2023-07-28 17:45:45 -0600

    accept invite and redirect user to new brewery

--- src/components/Invite/AcceptInvite.tsx
+++ src/components/Invite/AcceptInvite.tsx
@@ -1,7 +1,13 @@
+"use client";
+import { useBreweryContext } from "@/context/brewery-beer";
 import { acceptInvite } from "@/lib/POST/acceptInvite";
-import { set } from "mongoose";
-import { useRouter } from "next/router";
+import getSingleBrewery from "@/lib/getSingleBrewery";
+import { useSession } from "next-auth/react";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import path from "path";
 import React, { useEffect, useState } from "react";
+import useSWR from "swr";
 
 type Props = {};
 
@@ -9,13 +15,37 @@ const AcceptInvite = (props: Props) => {
   const router = useRouter();
   const [loading, setLoading] = useState(true);
   const [message, setMessage] = useState("");
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { setSelectedBrewery } = useBreweryContext();
+  const { data: session, update } = useSession();
 
-  const fetchInvite = async (token: string | string[]) => {
+  console.log({ pathname });
+  const fetchInvite = async (token: string) => {
     setLoading(true);
-    try {
-      const response = await acceptInvite({ token });
 
-      setMessage(response.message);
+    try {
+      if (session?.user.accessToken) {
+        const response = await acceptInvite({
+          token,
+          accessToken: session?.user.accessToken,
+        });
+
+        console.log({ response });
+
+        if (response.message === "Invitation accepted.") {
+          localStorage.setItem("selectedBrewery", response.brewery._id);
+          setSelectedBrewery(response.brewery);
+          await update({ newBreweryId: response.brewery._id });
+          router.push(`/breweries/${response.brewery._id}`);
+        } else {
+          // alert(response.message);
+          // setMessage(response.message);
+          // router.push("/");
+        }
+      } else {
+        // router.push("/"); // Redirect to the home page if the user is not authenticate
+      }
     } catch (err) {
       setMessage(err.message);
     } finally {
@@ -24,11 +54,17 @@ const AcceptInvite = (props: Props) => {
   };
 
   useEffect(() => {
-    const { token } = router.query;
+    // accept-invite url example:
+    // const inviteUrl = `http://localhost:3000/accept-invite?token=${token}`;
+
+    const token = searchParams.get("token");
+
     if (token) {
+      console.log("Token useEffect", { pathname });
+
       fetchInvite(token);
     }
-  }, [router.query]);
+  }, [pathname, session]);
 
   return <div>{loading ? <p>Loading...</p> : <p>{message}</p>}</div>;
 };
