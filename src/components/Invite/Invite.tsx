adabb483d0c3cb08ed07c31c8469b7b09ccec1ed
jbettmann <jordan.bettmann@gmail.com>
2023-07-28 17:45:53 -0600

    added error

--- src/components/Invite/Invite.tsx
+++ src/components/Invite/Invite.tsx
@@ -2,27 +2,34 @@
 import React from "react";
 
 type Props = {
-  invitee: { email: string; admin: boolean };
+  invitee: { email: string; isAdmin: boolean; error: string };
   handleChange: (field: string, value: string | boolean) => void;
   className?: string;
+  error: string;
 };
 
-const Invite = ({ invitee, handleChange, className }: Props) => {
+const Invite = ({ invitee, handleChange, className, error }: Props) => {
   return (
-    <div>
-      <input
-        type="email"
-        placeholder="Email"
-        className={`${className} form__input`}
-        value={invitee.email}
-        onChange={(e) => handleChange("email", e.target.value)}
-      />
-      <input
-        type="checkbox"
-        checked={invitee.admin}
-        onChange={(e) => handleChange("admin", e.target.checked)}
-      />
-      <label className="text-white">Admin?</label>
+    <div className="flex justify-centeritems-center">
+      <div className="text-center relative">
+        <input
+          type="email"
+          placeholder="Email"
+          className={`${className} form__input mb-6 `}
+          value={invitee.email}
+          onChange={(e) => handleChange("email", e.target.value)}
+        />
+        {error && <p className="error absolute right-12 top-9 ">{error}</p>}
+      </div>
+
+      <div className="flex justify-center items-center pb-7">
+        <input
+          type="checkbox"
+          checked={invitee.isAdmin}
+          onChange={(e) => handleChange("isAdmin", e.target.checked)}
+        />
+        <label className="text-white ml-2">Admin?</label>
+      </div>
     </div>
   );
 };
