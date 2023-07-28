784e64fa56fe24c63f2ae26ebfd670805aa2a4c9
jbettmann <jordan.bettmann@gmail.com>
2023-07-28 17:46:10 -0600

    updated endpoint

--- src/lib/POST/acceptInvite.ts
+++ src/lib/POST/acceptInvite.ts
@@ -1,22 +1,31 @@
 type pageProps = {
   token: string | string[];
+  accessToken: string;
 };
-export const acceptInvite = async ({ token }: pageProps) => {
+export const acceptInvite = async ({ token, accessToken }: pageProps) => {
+  console.log({ token, accessToken });
   try {
-    const response = await fetch(`/accept-invite?token=${token}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error(
-        `Invitation could not be accepted. ${response.statusText}`
+    if (token && accessToken) {
+      const response = await fetch(
+        `https://beer-bible-api.vercel.app/accept-invite?token=${token}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
       );
+
+      if (!response.ok) {
+        throw new Error(
+          `Invitation could not be accepted. ${response.statusText}`
+        );
+      }
+
+      const data = await response.json();
+      return data;
     }
-    const data = await response.json();
-    return data;
   } catch (err) {
     throw new Error(err.message);
   }
--- src/lib/POST/sendInvite.ts
+++ src/lib/POST/sendInvite.ts
@@ -7,23 +7,45 @@ interface Invite {
 type Props = {
   inviteData: Invite;
   breweryId: string;
+  accessToken: string;
 };
 
-export async function sendInvite({ inviteData, breweryId }: Props) {
-  const response = await fetch(`/breweries/${breweryId}/invite`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email: inviteData.email,
-      isAdmin: inviteData.isAdmin,
-    }),
-  });
+export async function sendInvite({
+  inviteData,
+  breweryId,
+  accessToken,
+}: Props) {
+  console.log({ inviteData, breweryId, accessToken });
+  if (breweryId && accessToken) {
+    const response = await fetch(
+      `https://beer-bible-api.vercel.app/breweries/${breweryId}/invite`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({
+          email: inviteData.email,
+          isAdmin: inviteData.isAdmin,
+        }),
+      }
+    );
 
-  if (!response.ok) {
-    throw new Error(response.statusText);
+    // Check if the response is JSON before calling response.json()
+    if (response.headers.get("Content-Type")?.includes("application/json")) {
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message || response.statusText);
+      }
+
+      return data;
+    } else {
+      // The response isn't JSON
+      const text = await response.text();
+
+      throw new Error(`Server responded with non-JSON response: ${text}`);
+    }
   }
-
-  return await response.json();
 }
