e65abd7bbad19c5c8b7ba70d2922df5236b426e6
jbettmann <jordan.bettmann@gmail.com>
2023-07-28 17:46:26 -0600

    added accept-invite path

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,6 +13,7 @@ export const config = {
     "/",
     "/api/:path*",
     "/api/message/:path*",
+    "/accept-invite/:path*",
     "/breweries/:path*",
     "/beers/:path*",
     "/create/:path*",
