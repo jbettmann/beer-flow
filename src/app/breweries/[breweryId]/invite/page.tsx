8a2675790a2108d12bc8a14c342ad0b56f25ef28
jbettmann <jordan.bettmann@gmail.com>
2023-07-28 17:45:05 -0600

    added breweryId

--- src/app/breweries/[breweryId]/invite/page.tsx
+++ src/app/breweries/[breweryId]/invite/page.tsx
@@ -1,5 +1,7 @@
 import MultipleInvites from "@/components/Invite/MultipuleInvites";
 import SetSelectedContainer from "@/components/SetSelectedContainer";
+import { redirect } from "next/navigation";
+
 import React from "react";
 type pageProps = {
   params: {
@@ -8,9 +10,10 @@ type pageProps = {
 };
 
 const InviteNewStaffPage = ({ params: { breweryId } }: pageProps) => {
+  if (!breweryId) redirect("/");
   return (
     <SetSelectedContainer breweryId={breweryId}>
-      <MultipleInvites />
+      <MultipleInvites breweryId={breweryId} />
     </SetSelectedContainer>
   );
 };
