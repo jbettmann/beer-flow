b1d25c1669d28da1a81fca0f122742b12fb0b236
jbettmann <jordan.bettmann@gmail.com>
2023-08-02 16:07:34 -0600

    Create BackArrow.tsx

---
+++ src/components/Buttons/BackArrow.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+type Props = {
+  width?: number;
+  height?: number;
+};
+
+const BackArrow = ({ width, height }: Props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width || "24"}
+      height={height || "24"}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="lucide lucide-move-left"
+    >
+      <path d="M6 8L2 12L6 16" />
+      <path d="M2 12H22" />
+    </svg>
+  );
+};
+
+export default BackArrow;
