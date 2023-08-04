7d3f90f8b167d219f80f81626ad5cb08dfd83568
jbettmann <jordan.bettmann@gmail.com>
2023-08-03 19:35:38 -0600

    Create loading.tsx

---
+++ src/app/@modal/(.)breweries/[breweryId]/categories/[categoryId]/loading.tsx
@@ -0,0 +1,10 @@
+import EditCategoryLS from "@/components/LoadingSkeleton/EditCategoryLS";
+import Modal from "@/components/Modal";
+
+export default function Loading() {
+  return (
+    <Modal>
+      <EditCategoryLS />
+    </Modal>
+  );
+}
