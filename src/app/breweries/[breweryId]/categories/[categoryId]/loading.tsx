61bffbbf6e7ee69c5ea4b8db9fe5f447729e296a
jbettmann <jordan.bettmann@gmail.com>
2023-08-03 19:35:40 -0600

    Create loading.tsx

---
+++ src/app/breweries/[breweryId]/categories/[categoryId]/loading.tsx
@@ -0,0 +1,9 @@
+import EditCategoryLS from "@/components/LoadingSkeleton/EditCategoryLS";
+
+export default function Loading() {
+  return (
+    <div className="w-1/2 m-auto">
+      <EditCategoryLS />
+    </div>
+  );
+}
