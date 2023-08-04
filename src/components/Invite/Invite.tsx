a5f081c392a60e6228d81e5e83dd6c18b7c2714a
jbettmann <jordan.bettmann@gmail.com>
2023-08-03 19:36:52 -0600

    replaced radio with toggle

--- src/components/Invite/Invite.tsx
+++ src/components/Invite/Invite.tsx
@@ -21,15 +21,26 @@ const Invite = ({ invitee, handleChange, className, error }: Props) => {
         />
         {error && <p className="error absolute right-12 top-9 ">{error}</p>}
       </div>
+      <div className="form-control">
+        <label className="cursor-pointer label w-fit">
+          <span className="label-text text-white mr-2">Make Admin</span>
+          <input
+            type="checkbox"
+            className="toggle toggle-accent"
+            onChange={(e) => handleChange("isAdmin", e.target.checked)}
+            checked={invitee.isAdmin}
+          />
+        </label>
+      </div>
 
-      <div className="flex justify-center items-center pb-7">
+      {/* <div className="flex justify-center items-center pb-7">
         <input
           type="checkbox"
           checked={invitee.isAdmin}
           onChange={(e) => handleChange("isAdmin", e.target.checked)}
         />
         <label className="text-white ml-2">Admin?</label>
-      </div>
+      </div> */}
     </div>
   );
 };
