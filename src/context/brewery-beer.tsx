8a06735bf990683e81fd1b6dc24277f821c8a63c
jbettmann <jordan.bettmann@gmail.com>
2023-08-08 15:08:38 -0600

    set Brewery and Beer state

--- src/context/brewery-beer.tsx
+++ src/context/brewery-beer.tsx
@@ -1,6 +1,18 @@
-import React, { createContext, useState, useContext, FC } from "react";
+"use client";
+import React, {
+  createContext,
+  useState,
+  useContext,
+  FC,
+  use,
+  useEffect,
+} from "react";
 import { Beer } from "@/app/types/beer";
 import { Brewery } from "@/app/types/brewery";
+import useSWR from "swr";
+import { useSession } from "next-auth/react";
+import getBreweryBeers from "@/lib/getBreweryBeers";
+import getSingleBrewery from "@/lib/getSingleBrewery";
 
 type BreweryContextProps = {
   selectedBrewery: Brewery | null;
@@ -19,6 +31,64 @@ type ProviderProps = {
 export const BreweryProvider: FC<ProviderProps> = ({ children }) => {
   const [selectedBrewery, setSelectedBrewery] = useState<Brewery | null>(null);
   const [selectedBeers, setSelectedBeers] = useState<Beer[] | null>(null);
+
+  const { data: session } = useSession();
+  const [breweryId, setBreweryId] = useState<string | null>(
+    localStorage.getItem("selectedBreweryId") || null
+  );
+  const { data: beers, error: beersError } = useSWR(
+    [
+      `https://beer-bible-api.vercel.app/breweries/${breweryId}/beers`,
+      session?.user.accessToken,
+    ],
+    getBreweryBeers
+  );
+
+  const { data: brewery, error: breweryError } = useSWR(
+    [
+      `https://beer-bible-api.vercel.app/breweries/${breweryId}`,
+      session?.user.accessToken,
+    ],
+    getSingleBrewery
+  );
+
+  useEffect(() => {
+    // Handler for the custom event
+    const handleSelectedBreweryChanged = () => {
+      const breweryId = localStorage.getItem("selectedBreweryId");
+      setBreweryId(breweryId);
+    };
+
+    // Add the event listener
+    window.addEventListener(
+      "selectedBreweryChanged",
+      handleSelectedBreweryChanged
+    );
+
+    // Cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener(
+        "selectedBreweryChanged",
+        handleSelectedBreweryChanged
+      );
+    };
+  }, []); // Empty dependency array, so this runs only on mount and unmount
+
+  // Watch for changes in breweryId and synchronize with local storage
+  useEffect(() => {
+    if (breweryId) {
+      localStorage.setItem("selectedBreweryId", breweryId);
+    } else {
+      localStorage.removeItem("selectedBreweryId");
+    }
+  }, [breweryId]);
+
+  useEffect(() => {
+    setSelectedBeers(beers);
+    setSelectedBrewery(brewery);
+  }, [beers, brewery]);
+
+  console.log("From BreweryContext", { brewery, beers });
 
   return (
     <BreweryContext.Provider
