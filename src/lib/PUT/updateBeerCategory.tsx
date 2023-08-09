55f80ad86321b8aa0f084f5ff6ed67e5841c2794
jbettmann <jordan.bettmann@gmail.com>
2023-08-09 16:45:46 -0600

    Create updateBeerCategory.tsx

---
+++ src/lib/PUT/updateBeerCategory.tsx
@@ -0,0 +1,46 @@
+import { Beer } from "@/app/types/beer";
+
+type pageProps = {
+  updatedCategory: string[];
+  accessToken: string | undefined;
+  breweryId: string | undefined;
+  beerId: string;
+};
+
+export default async function updateBeerCategory({
+  updatedCategory,
+  breweryId,
+  beerId,
+  accessToken,
+}: pageProps) {
+  if (accessToken) {
+    console.log({ updatedCategory, breweryId, beerId, accessToken });
+    try {
+      const response = await fetch(
+        `https://beer-bible-api.vercel.app/breweries/${breweryId}/beers/${beerId}/category`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+          body: JSON.stringify({ category: updatedCategory }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      // Call the `updateBeerCategory` function for each updated beer
+
+      const responseData: Beer = await response.json();
+
+      return responseData;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  } else {
+    throw new Error("User session not found.");
+  }
+}
