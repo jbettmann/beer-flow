2f8f4db3db92033a4c3f9f81a1c4ceffbee71e25
jbettmann <jordan.bettmann@gmail.com>
2023-08-10 16:45:39 -0600

    Create handleMoveBeerToCategory.tsx

---
+++ src/lib/handleSubmit/handleMoveBeerToCategory.tsx
@@ -0,0 +1,82 @@
+import { Brewery } from "@/app/types/brewery";
+import { FormValues } from "@/components/CreateBeerForm/types";
+import updateBeer from "../PUT/updateBeer";
+import createCategory from "../createCategory";
+import getSingleBrewery from "../getSingleBrewery";
+import { revalidatePath } from "next/cache";
+import updateBeerCategory from "../PUT/updateBeerCategory";
+
+type Props = {
+  values: FormValues | [];
+  beerId: string | null;
+  brewery: Brewery | null;
+  accessToken: string | undefined;
+  updatedCategory: string[];
+};
+// Handle form submission
+const handleMoveBeerToCategory = async ({
+  values,
+  beerId,
+  brewery,
+  accessToken,
+  updatedCategory,
+}: Props) => {
+  try {
+    console.log(
+      { brewery, beerId, accessToken, updatedCategory },
+      values.category
+    );
+    // Function to get category ID, creating a new category if necessary
+
+    // Converting brewery categories to a Map for O(1) lookup times
+    const existingCategories = new Map(
+      brewery?.categories.map((cat) => [cat.name, cat._id])
+    );
+
+    // Function to get category ID, creating a new category if necessary
+    const getCategoryId = async (categoryName: string): Promise<string> => {
+      if (existingCategories.has(categoryName)) {
+        const existingId = existingCategories.get(categoryName);
+        if (!existingId) {
+          throw new Error(
+            `ID not found for existing category: ${categoryName}`
+          );
+        }
+        return existingId;
+      } else {
+        const newCategory = { name: categoryName };
+
+        const createdCategory = await createCategory({
+          newCategory,
+          breweryId: brewery?._id,
+          accessToken,
+        });
+        if (!createdCategory._id) {
+          throw new Error(`Category creation failed for: ${categoryName}`);
+        }
+        return createdCategory._id;
+      }
+    };
+
+    // Map categories to their IDs
+    const categoryIds = await Promise.all(
+      values.category.map((category) => getCategoryId(category.value))
+    );
+
+    const newCategoryIds = [...categoryIds, ...updatedCategory];
+
+    console.log({ newCategoryIds });
+
+    const updatedBeerRes = await updateBeerCategory({
+      beerId,
+      updatedCategory: newCategoryIds,
+      breweryId: brewery?._id,
+      accessToken,
+    });
+
+    return updatedBeerRes;
+  } catch (error) {
+    console.error(error);
+  }
+};
+export default handleMoveBeerToCategory;
