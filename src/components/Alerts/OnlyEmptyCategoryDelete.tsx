8a45a0cab8e78ade75e3ec176ca2ffae265368cb
jbettmann <jordan.bettmann@gmail.com>
2023-08-11 21:55:43 -0600

    Create OnlyEmptyCategoryDelete.tsx

---
+++ src/components/Alerts/OnlyEmptyCategoryDelete.tsx
@@ -0,0 +1,89 @@
+"use client";
+import { Category } from "@/app/types/category";
+import React, { useEffect, useRef, useState } from "react";
+import Modal from "../Modal";
+
+type Props = {
+  category: Category;
+  setAlertOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setToContinue: React.Dispatch<React.SetStateAction<boolean>>;
+  alertOpen: boolean;
+};
+
+const OnlyEmptyCategoryDelete = ({
+  alertOpen,
+  setToContinue,
+  setAlertOpen,
+}: Props) => {
+  const modalRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (alertOpen) {
+      if (modalRef.current) {
+        modalRef.current.showModal();
+      }
+    }
+    console.log({ modalRef });
+  }, [alertOpen]);
+
+  const closeModal = () => {
+    console.log("Close modal called"); // Debugging
+    if (modalRef.current) {
+      modalRef.current.close();
+      setAlertOpen(false);
+    }
+  };
+  return (
+    <dialog
+      ref={modalRef}
+      id="my_modal_5"
+      className="modal modal-bottom sm:modal-middle"
+    >
+      <form method="dialog" className="modal-box p-0">
+        <div className="p-6 flex flex-col alert">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+            />
+          </svg>
+          <span>{`Only Categories that are empty will be deleted`}</span>
+
+          <div>
+            <button
+              className="btn btn-sm"
+              onClick={(e) => {
+                e.preventDefault();
+                setToContinue(false);
+                closeModal();
+              }}
+            >
+              Cancel
+            </button>
+            <button
+              className="ml-2 btn btn-sm btn-accent"
+              onClick={(e) => {
+                console.log("Delete button clicked!"); // Debugging
+                e.preventDefault();
+                setToContinue(true);
+                console.log("setToContinue called with true"); // Debugging
+                closeModal();
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </form>
+    </dialog>
+  );
+};
+
+export default OnlyEmptyCategoryDelete;
