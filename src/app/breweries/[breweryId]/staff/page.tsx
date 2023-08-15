f6531a12cd2bf94d8b03a74b31ae71b62911db54
jbettmann <jordan.bettmann@gmail.com>
2023-08-15 15:03:48 -0600

    Create page.tsx

---
+++ src/app/breweries/[breweryId]/staff/page.tsx
@@ -0,0 +1,13 @@
+import StaffTable from "@/components/StaffManagement/StaffTable";
+
+type Props = {};
+
+const BreweryStaffPage = (props: Props) => {
+  return (
+    <div className="p-8">
+      <StaffTable />
+    </div>
+  );
+};
+
+export default BreweryStaffPage;
