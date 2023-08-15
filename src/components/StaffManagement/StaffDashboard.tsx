64b51bf93921d8bce568c5691ff7b8d4131b263c
jbettmann <jordan.bettmann@gmail.com>
2023-08-15 15:03:44 -0600

    Create StaffDashboard.tsx

---
+++ src/components/StaffManagement/StaffDashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const StaffDashboard = (props: Props) => {
+  return (
+    <div className="flex-auto w-64  card bg-base-100 shadow-md">
+      StaffDashboard
+    </div>
+  );
+};
+
+export default StaffDashboard;
