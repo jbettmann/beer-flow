432f2607438cc97814adc43c1af2621c4f5bf4ac
jbettmann <jordan.bettmann@gmail.com>
2023-08-16 16:50:33 -0600

    added responsive menu for small and large screen

--- src/components/StaffManagement/StaffDashboard.tsx
+++ src/components/StaffManagement/StaffDashboard.tsx
@@ -1,12 +1,49 @@
+"use client";
 import React from "react";
 
-type Props = {};
+type Props = {
+  viewFilter: string;
+  setViewFilter: React.Dispatch<React.SetStateAction<string>>;
+};
 
-const StaffDashboard = (props: Props) => {
+const StaffDashboard = ({ viewFilter, setViewFilter }: Props) => {
+  const menuButtons = ["All Staff", "Admin", "Crew"];
   return (
-    <div className="flex-auto w-64  card bg-base-100 shadow-md">
-      StaffDashboard
-    </div>
+    <>
+      {/* Large screen side dashboard menu */}
+      <div className="hidden lg:flex-initial lg:w-48 lg:card lg:bg-base-100 lg:shadow-md lg:space-y-2">
+        {menuButtons.map((button) => (
+          <button
+            className="btn btn-ghost justify-start"
+            onClick={() => setViewFilter(button)}
+          >
+            {button}
+          </button>
+        ))}
+      </div>
+
+      {/* Small screen dropdown menu */}
+      <div className="lg:hidden flex-initial w-48 z-[1] lg:z-0 card bg-base-100 shadow-md space-y-2 dropdown ">
+        <label className="btn btn-ghost w-full" tabIndex={0}>
+          {viewFilter}
+        </label>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          {menuButtons.map((button) => (
+            <li>
+              <button
+                className="btn btn-ghost "
+                onClick={() => setViewFilter(button)}
+              >
+                {button}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
