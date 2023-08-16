73c4687f3d93876217bcd4755605b02fa0c17d88
jbettmann <jordan.bettmann@gmail.com>
2023-08-16 16:51:07 -0600

    added functionally to table rows

--- src/components/StaffManagement/StaffTable.tsx
+++ src/components/StaffManagement/StaffTable.tsx
@@ -1,40 +1,61 @@
 "use client";
+import { Users } from "@/app/types/users";
 import { useBreweryContext } from "@/context/brewery-beer";
-import { useSession } from "next-auth/react";
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import Image from "next/image";
-import StaffDashboard from "./StaffDashboard";
+import { Brewery } from "@/app/types/brewery";
+import { PencilLine } from "lucide-react";
+import StaffMemberRow from "./StaffMemberRow";
 
-type Props = {};
+type Props = {
+  viewFilter: string;
+  brewery: Brewery | null;
+};
 
-const StaffTable = (props: Props) => {
-  const { data: session } = useSession();
-  const { selectedBrewery } = useBreweryContext();
-  console.log({ selectedBrewery }, session?.user);
-  {
-    selectedBrewery?.staff.length !== 0 ? (
-      selectedBrewery?.staff.map((staff) => (
-        <>
-          <Image
-            src={staff.image}
-            alt={`profile picture of ${staff.fullName}`}
-            className="beer-category__image"
-            width={50}
-            height={50}
-          />
-          <p key={staff._id}>{staff.fullName}</p>
-        </>
-      ))
-    ) : (
-      <p>No staff</p>
-    );
-  }
+const StaffTable = ({ viewFilter, brewery }: Props) => {
+  const [staff, setStaff] = useState<Users[] | string[] | number | any>(
+    brewery?.staff
+  );
+
+  const [checkedStaffIds, setCheckedStaffIds] = useState<Set<string>>(
+    new Set()
+  );
+
+  const adminIds = useMemo(
+    () => new Set(brewery?.admin.map((admin) => admin._id)),
+    [brewery]
+  );
+
+  const handleCheckboxChange = (id: string) => {
+    const newCheckedStaffIds = new Set(checkedStaffIds);
+    if (!newCheckedStaffIds.has(id)) {
+      newCheckedStaffIds.add(id);
+    } else {
+      newCheckedStaffIds.delete(id);
+    }
+    setCheckedStaffIds(newCheckedStaffIds);
+  };
+
+  useEffect(() => {
+    if (brewery) {
+      if (viewFilter === "All Staff")
+        setStaff([...brewery.staff, brewery.owner]);
+      if (viewFilter === "Admin") setStaff([...brewery.admin, brewery.owner]);
+      if (viewFilter === "Crew")
+        setStaff(
+          brewery.staff.filter(
+            (crew) => crew._id !== brewery?.owner._id && !adminIds.has(crew._id)
+          )
+        );
+    }
+  }, [viewFilter]);
+
+  console.log({ checkedStaffIds });
+
   return (
-    <div className="flex flex-col w-full lg:flex-row">
-      <StaffDashboard />
-
+    brewery && (
       <div className="overflow-x-auto flex-auto lg:pl-8">
-        <table className="table">
+        <table className="table border-separate border-spacing-y-6 p-3">
           {/* head */}
           <thead>
             <tr>
@@ -44,166 +65,54 @@ const StaffTable = (props: Props) => {
                 </label>
               </th>
               <th>Name</th>
-              <th>Position</th>
-              <th>Favorite Color</th>
-              <th></th>
+              <th>Role</th>
+
+              <th className="text-right">Manage</th>
             </tr>
           </thead>
-          <tbody>
-            {/* row 1 */}
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/tailwind-css-component-profile-2@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Hart Hagerty</div>
-                    <div className="text-sm opacity-50">United States</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Zemlak, Daniel and Leannon
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Desktop Support Technician
-                </span>
-              </td>
-              <td>Purple</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            {/* row 2 */}
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/tailwind-css-component-profile-3@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Brice Swyre</div>
-                    <div className="text-sm opacity-50">China</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Carroll Group
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Tax Accountant
-                </span>
-              </td>
-              <td>Red</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            {/* row 3 */}
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/tailwind-css-component-profile-4@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Marjy Ferencz</div>
-                    <div className="text-sm opacity-50">Russia</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Rowe-Schoen
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Office Assistant I
-                </span>
-              </td>
-              <td>Crimson</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            {/* row 4 */}
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/tailwind-css-component-profile-5@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Yancy Tear</div>
-                    <div className="text-sm opacity-50">Brazil</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Wyman-Ledner
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Community Outreach Specialist
-                </span>
-              </td>
-              <td>Indigo</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
+          <tbody className="">
+            {staff && staff.length !== 0 ? (
+              staff.map((s) => {
+                const role =
+                  s._id === brewery?.owner._id
+                    ? "Owner"
+                    : adminIds.has(s._id)
+                    ? "Admin"
+                    : "Crew";
+                return (
+                  <StaffMemberRow
+                    key={s._id}
+                    staff={s}
+                    role={role}
+                    admin={adminIds.has(s._id)}
+                    handleCheckboxChange={handleCheckboxChange}
+                    checkedStaffIds={checkedStaffIds}
+                  />
+                );
+              })
+            ) : (
+              <tr>
+                <th></th>
+                <td>
+                  <p>No staff</p>
+                </td>
+                <th></th>
+              </tr>
+            )}
           </tbody>
           {/* foot */}
           <tfoot>
             <tr>
               <th></th>
               <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
-              <th></th>
+              <th>Role</th>
+
+              <th className="text-right">Manage</th>
             </tr>
           </tfoot>
         </table>
       </div>
-    </div>
+    )
   );
 };
 
