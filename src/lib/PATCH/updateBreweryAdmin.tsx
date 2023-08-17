5fde86e662f6037e56c02feee0a1ba57217fc79b
jbettmann <jordan.bettmann@gmail.com>
2023-08-17 14:20:01 -0600

    Create updateBreweryAdmin.tsx

---
+++ src/lib/PATCH/updateBreweryAdmin.tsx
@@ -0,0 +1,47 @@
+import { Beer } from "@/app/types/beer";
+import { Brewery } from "@/app/types/brewery";
+
+type pageProps = {
+  userId: string | undefined;
+  accessToken: string | undefined;
+  breweryId: string | undefined;
+  action: string | null;
+};
+
+export default async function updateBreweryAdmin({
+  userId,
+  breweryId,
+  action,
+  accessToken,
+}: pageProps) {
+  if (accessToken && breweryId) {
+    console.log("updateBreweryAdmin", action, breweryId);
+    try {
+      const response = await fetch(
+        `https://beer-bible-api.vercel.app/breweries/${breweryId}/admins/${userId}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+          body: JSON.stringify({ action: action }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      const responseData = await response.json();
+
+      console.log(responseData);
+      return responseData;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  } else {
+    throw new Error("Brewery not found.");
+  }
+}
