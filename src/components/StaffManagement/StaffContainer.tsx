ac143fda06edad8831916bfeb4dd15eeb3dfaf87
jbettmann <jordan.bettmann@gmail.com>
2023-08-22 15:16:19 -0600

    added invite new staff button

--- src/components/StaffManagement/StaffContainer.tsx
+++ src/components/StaffManagement/StaffContainer.tsx
@@ -4,22 +4,33 @@ import { useSession } from "next-auth/react";
 import React, { useState } from "react";
 import StaffDashboard from "./StaffDashboard";
 import StaffTable from "./StaffTable";
+import Link from "next/link";
+import { UserPlus } from "lucide-react";
 
 type Props = {};
 
 const StaffContainer = (props: Props) => {
-  const { data: session } = useSession();
   const { selectedBrewery } = useBreweryContext();
   const [viewFilter, setViewFilter] = useState<string>("All Staff");
 
   return (
     selectedBrewery?.staff && (
       <>
-        {/* <h1 className="text-left">{selectedBrewery.companyName} Staff</h1>
-        <div className="text-sm opacity-50">
-          Owner {selectedBrewery.owner.fullName || ""}
-        </div> */}
-        <div className="flex flex-col w-full lg:flex-row">
+        <div className="flex justify-between p-5">
+          <div className="flex flex-col w-fit">
+            <h3 className="text-left">{selectedBrewery.companyName} Staff</h3>
+            <div className="text-sm badge badge-ghost opacity-50 mt-2">
+              Owner {selectedBrewery.owner.fullName || ""}
+            </div>
+          </div>
+          <Link
+            href={`/breweries/${selectedBrewery?._id}/invite`}
+            className="btn btn-accent"
+          >
+            Invite <UserPlus size={20} />
+          </Link>
+        </div>
+        <div className="flex flex-col w-full lg:flex-row p-5">
           <StaffDashboard
             setViewFilter={setViewFilter}
             viewFilter={viewFilter}
