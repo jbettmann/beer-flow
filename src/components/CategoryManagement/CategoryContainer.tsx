bb69fa4d4a7c0b63227e06f5d9b19e7507968e9d
jbettmann <jordan.bettmann@gmail.com>
2023-08-22 15:12:42 -0600

    Create CategoryContainer.tsx

---
+++ src/components/CategoryManagement/CategoryContainer.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { useBreweryContext } from "@/context/brewery-beer";
+import { useSession } from "next-auth/react";
+import React, { useState } from "react";
+import StaffDashboard from "./StaffDashboard";
+import StaffTable from "./StaffTable";
+import Link from "next/link";
+import { UserPlus } from "lucide-react";
+import CategoryDashboard from "./CategoryDashboard";
+import CategoryList from "./CategoryList";
+
+type Props = {};
+
+const CategoryContainer = (props: Props) => {
+  const { selectedBrewery } = useBreweryContext();
+  const [createNewCategory, setCreateNewCategory] = useState<boolean>(false);
+  const [viewFilter, setViewFilter] = useState<string>("All Categories");
+  console.log({ createNewCategory });
+  return (
+    selectedBrewery?.staff && (
+      <>
+        <div className="flex justify-between p-5">
+          <div className="flex flex-col w-fit">
+            <h3 className="text-left">Categories</h3>
+            {/* <div className="text-sm badge badge-ghost opacity-50 mt-2">
+              Owner {selectedBrewery.owner.fullName || ""}
+            </div> */}
+          </div>
+          <button
+            onClick={() => setCreateNewCategory(true)}
+            className="btn btn-accent"
+          >
+            + New Category
+          </button>
+        </div>
+        <div className="flex flex-col w-full lg:flex-row p-5">
+          <CategoryDashboard
+            setViewFilter={setViewFilter}
+            viewFilter={viewFilter}
+          />
+          <CategoryList
+            viewFilter={viewFilter}
+            brewery={selectedBrewery}
+            createNewCategory={createNewCategory}
+            setCreateNewCategory={setCreateNewCategory}
+          />
+        </div>
+      </>
+    )
+  );
+};
+
+export default CategoryContainer;
