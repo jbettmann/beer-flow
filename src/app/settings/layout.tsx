5c95be9882f5f9c033fd4f5dd2c9c8ca31d468ca
jbettmann <jordan.bettmann@gmail.com>
2023-08-28 14:15:18 -0600

    Create layout.tsx

---
+++ src/app/settings/layout.tsx
@@ -0,0 +1,15 @@
+import SettingTabs from "@/components/Settings/SettingTabs";
+import Link from "next/link";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default async function BreweriesIdLayout({ children }: Props) {
+  return (
+    <div className=" p-3 md:p-8">
+      <SettingTabs />
+      <div className=" lg:w-3/4 px-10">{children}</div>
+    </div>
+  );
+}
