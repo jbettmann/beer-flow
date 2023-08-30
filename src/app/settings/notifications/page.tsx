e914a4e69eec7662c3a9c2e322b2500970383c3a
jbettmann <jordan.bettmann@gmail.com>
2023-08-30 16:04:31 -0600

    Update page.tsx

--- src/app/settings/notifications/page.tsx
+++ src/app/settings/notifications/page.tsx
@@ -1,9 +1,10 @@
+import NotificationSettings from "@/components/Settings/NotificationSettings";
 import React from "react";
 
 type Props = {};
 
 const NotificationsPage = (props: Props) => {
-  return <div>NotificationsPage</div>;
+  return <NotificationSettings />;
 };
 
 export default NotificationsPage;
