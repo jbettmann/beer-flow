4b8bf68215786e7d9e9afccc5786fff4b0ad75d0
jbettmann <jordan.bettmann@gmail.com>
2023-08-30 16:05:22 -0600

    make reuseable component

--- src/components/Settings/DeleteAccount.tsx
+++ src/components/Settings/DeleteAccount.tsx
@@ -1,21 +1,19 @@
 "use client";
 import React from "react";
+import DeleteOrRemoveButton from "../Buttons/DeleteOrRemoveButton";
 
 type Props = {};
 
+// Components created to use on setting page and use reuseable DeleteOrRemoveButton component
 const DeleteAccount = (props: Props) => {
   return (
-    <div>
-      <button
-        onClick={() => console.log("Delete Account")}
-        className="btn btn-error btn-outline"
-      >
-        Delete Account
-      </button>
-      <p className="mx-1 text-sm opacity-50">
-        Permanently delete account and all breweries you own.
-      </p>
-    </div>
+    <DeleteOrRemoveButton
+      onClick={() => console.log("Delete Account")}
+      buttonClassName="btn btn-error btn-outline"
+      title="Delete Account"
+      description="Permanently delete account and all breweries you own."
+      descriptionClassName="mx-1 text-sm opacity-50"
+    />
   );
 };
 
