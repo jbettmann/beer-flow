8eb4cfab0675e65a54e0e03d52d4fe813a51fc51
jbettmann <jordan.bettmann@gmail.com>
2023-08-30 16:04:00 -0600

    added notifications

--- models/user.ts
+++ models/user.ts
@@ -1,14 +1,31 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 // user schema
-const userSchema = new Schema({
-  fullName: { type: String, required: true },
-  email: { type: String, required: true },
-  breweries: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "Brewery", default: [] },
-  ],
-  image: String,
-});
+const userSchema = new Schema(
+  {
+    fullName: { type: String, required: true },
+    email: { type: String, required: true },
+    breweries: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "Brewery", default: [] },
+    ],
+    image: String,
+    notifications: {
+      allow: { type: Boolean, default: true }, // or false
+      newBeerRelease: {
+        email: { type: Boolean, default: true },
+        push: { type: Boolean, default: true },
+      },
+      beerUpdate: {
+        email: { type: Boolean, default: true },
+        push: { type: Boolean, default: true },
+      },
+      // ... more detailed preferences if needed
+    },
+  },
+  {
+    timestamps: true, // This will create the `createdAt` and `updatedAt` fields automatically
+  }
+);
 const User = mongoose.models?.User || mongoose.model("User", userSchema);
 
 export default User;
