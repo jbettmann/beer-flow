6c96863c96b06748398425fdb7950df2df03a4d2
jbettmann <jordan.bettmann@gmail.com>
2023-09-08 15:12:09 -0600

    added optional className

--- src/components/Buttons/SaveButton.tsx
+++ src/components/Buttons/SaveButton.tsx
@@ -3,11 +3,12 @@ import React, { MouseEventHandler } from "react";
 type Props = {
   isLoading: boolean;
   onClick?: (e: Event) => void;
+  className?: string;
 };
 
-const SaveButton = ({ isLoading, onClick }: Props) => {
+const SaveButton = ({ isLoading, onClick, className }: Props) => {
   return (
-    <button className="create-btn" onClick={onClick}>
+    <button className={className ? className : "create-btn"} onClick={onClick}>
       {isLoading ? (
         <span className="loading loading-spinner text-accent"></span>
       ) : (
