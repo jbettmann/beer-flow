09dbdef6c852cbe6527798feb8dbc0795dbad298
jbettmann <jordan.bettmann@gmail.com>
2023-09-13 14:49:32 -0600

    Update layout.tsx

--- src/app/breweries/layout.tsx
+++ src/app/breweries/layout.tsx
@@ -1,16 +1,5 @@
-import { Metadata } from "next";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-import { compareSync } from "bcrypt";
-
 export default async function BreweriesLayout(props: {
   children: React.ReactNode;
-  modal: React.ReactNode;
 }) {
-  return (
-    <div className="h-screen">
-      {props.children}
-      {props.modal}
-    </div>
-  );
+  return <div className="h-screen">{props.children}</div>;
 }
