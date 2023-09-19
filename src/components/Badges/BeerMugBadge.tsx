1e9bc77b69c7c559443a65db6e5494adc7b26df7
jbettmann <jordan.bettmann@gmail.com>
2023-09-19 15:18:01 -0600

    updated for custom styling

--- src/components/Badges/BeerMugBadge.tsx
+++ src/components/Badges/BeerMugBadge.tsx
@@ -4,11 +4,18 @@ import Image from "next/image";
 
 type Props = {
   beerCount: number;
+  className?: string;
 };
 
-const BeerMugBadge = ({ beerCount }: Props) => {
+const BeerMugBadge = ({ beerCount, className }: Props) => {
   return (
-    <div className="bg-[#e5d773] bg-opacity-80 flex justify-center items-center rounded-full h-1/3 p-1">
+    <div
+      className={
+        className
+          ? className
+          : "bg-[#e5d773] bg-opacity-80 flex justify-center items-center rounded-full h-1/3 p-1 px-2"
+      }
+    >
       <p className="text-third-color m-0 text-xs tra">{beerCount}</p>
       <Image src={BeerMug} alt="beer mug badge" width={15} height={15} />
     </div>
