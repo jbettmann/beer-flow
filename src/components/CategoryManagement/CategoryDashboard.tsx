d71ada134d30ad6fb5b2d9e2b073912b5cb76282
jbettmann <jordan.bettmann@gmail.com>
2023-09-21 15:18:46 -0600

    update styling and full screen

--- src/components/CategoryManagement/CategoryDashboard.tsx
+++ src/components/CategoryManagement/CategoryDashboard.tsx
@@ -1,21 +1,25 @@
 "use client";
-import React from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 
 type Props = {
   viewFilter: string;
-  setViewFilter: React.Dispatch<React.SetStateAction<string>>;
+  setViewFilter: Dispatch<SetStateAction<string>>;
 };
 
 const CategoryDashboard = ({ viewFilter, setViewFilter }: Props) => {
-  const menuButtons = ["All Categories", "Empty Categories"];
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  console.log(isOpen);
+  const menuButtons = ["All", "Empty"];
   return (
     <>
       {/* Large screen side dashboard menu */}
-      <div className="hidden lg:flex-initial lg:w-48 lg:h-[65vh] lg:card bg-third-color lg:shadow-md lg:space-y-2">
+      <div className="hidden lg:flex-initial dashboard__container  py-7 px-1 lg:w-fit lg:h-[65vh] lg:card">
         {menuButtons.map((button, i) => (
           <button
             key={i}
-            className="btn btn-ghost justify-start"
+            className={`btn btn-ghost rounded-sm hover:bg-the-gray hover:text-accent justify-center ${
+              viewFilter === button ? "bg-the-gray  text-accent" : ""
+            }`}
             onClick={() => setViewFilter(button)}
           >
             {button}
@@ -24,19 +28,25 @@ const CategoryDashboard = ({ viewFilter, setViewFilter }: Props) => {
       </div>
 
       {/* Small screen dropdown menu */}
-      <div className="lg:hidden flex-initial w-48 z-[1] lg:z-0 card bg-third-color  shadow-md space-y-2 dropdown ml-3">
-        <label className="btn btn-ghost w-full" tabIndex={0}>
-          {viewFilter}
-        </label>
+      <div
+        className={`lg:hidden flex-initial card dashboard__container w-full z-[1] lg:z-0  dropdown ${
+          isOpen ? "dropdown-open" : ""
+        } ml-3`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <label className="btn btn-ghost w-full">{viewFilter}</label>
         <ul
-          tabIndex={0}
-          className="dropdown-content menu p-2 shadow bg-fourth-color rounded-box w-52"
+          className={`dropdown-content menu p-2 shadow bg-fourth-color rounded-box w-full`}
         >
           {menuButtons.map((button, i) => (
             <li key={i}>
               <button
                 className="btn btn-ghost "
-                onClick={() => setViewFilter(button)}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setIsOpen(!isOpen);
+                  setViewFilter(button);
+                }}
               >
                 {button}
               </button>
