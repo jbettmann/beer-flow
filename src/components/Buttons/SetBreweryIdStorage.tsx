1c55e46035e2551b0f0e136dddcd3d49906ba505
jbettmann <jordan.bettmann@gmail.com>
2023-09-25 14:27:03 -0600

    added brewery logo

--- src/components/Buttons/SetBreweryIdStorage.tsx
+++ src/components/Buttons/SetBreweryIdStorage.tsx
@@ -2,6 +2,8 @@
 import { Brewery } from "@/app/types/brewery";
 import Link from "next/link";
 import React from "react";
+import ImageDisplay from "../ImageDisplay/ImageDisplay";
+import { getInitials } from "@/lib/utils";
 
 type Props = {
   href: string;
@@ -19,8 +21,21 @@ const SetBreweryIdStorage = ({ href, brewery }: Props) => {
     window.dispatchEvent(selectedBreweryChangedEvent);
   };
   return (
-    <Link href={href} onClick={handleBreweryToStorage}>
-      {brewery.companyName}
+    <Link
+      href={href}
+      onClick={handleBreweryToStorage}
+      className="flex items-center justify-center gap-3"
+    >
+      {brewery?.image ? (
+        <ImageDisplay item={brewery} className="logo" />
+      ) : (
+        brewery?.companyName && (
+          <div className=" logo__default ">
+            {getInitials(brewery.companyName || "")}
+          </div>
+        )
+      )}
+      <h6 className="font-bold">{brewery.companyName}</h6>
     </Link>
   );
 };
