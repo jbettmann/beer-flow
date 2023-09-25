11877a05c8f645507deb906fd2b4e272fd40b520
jbettmann <jordan.bettmann@gmail.com>
2023-09-25 14:28:15 -0600

    updated input and stying

--- src/components/CategoryManagement/CreateNewCategoryRow.tsx
+++ src/components/CategoryManagement/CreateNewCategoryRow.tsx
@@ -10,38 +10,46 @@ const CreateNewCategoryRow = ({
   setCreateNewCategory,
   handleSaveNewCategory,
 }: Props) => {
+  //  Runs name change save on keydown "Enter"
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      const target = event.target as HTMLInputElement;
+      handleSaveNewCategory(target.value);
+      event.preventDefault(); // To prevent any default behavior, e.g., form submission
+    }
+  };
   return (
     <tr className={"shadow-sm"}>
       <th></th>
       <td>
         <div className="flex items-center space-x-3 ">
-          <label className="  btn btn-circle ">
-            {/* this hidden checkbox controls the state */}
-
-            <LayoutGrid size={24} strokeWidth={1} />
+          <label className=" ">
+            <LayoutGrid size={28} strokeWidth={1} className="fill-primary " />
           </label>
-          <div>
-            <div className="font-bold ">
-              <input
-                type="text"
-                className="input input-bordered input-accent"
-                autoFocus
-                placeholder="Enter category name"
-                onBlur={(e) => handleSaveNewCategory(e.target.value)}
-              />
-            </div>
+
+          <div className="font-bold ">
+            <input
+              type="text"
+              className="input font-semibold "
+              autoFocus
+              placeholder="Category name"
+              onBlur={(e) => handleSaveNewCategory(e.target.value)}
+              onKeyDown={handleKeyPress}
+            />
           </div>
         </div>
       </td>
-      <td></td>
 
       <th>
         <button
-          className="btn btn-outline btn-error"
+          className="btn btn-error  " // Made the button more visible
           onClick={() => setCreateNewCategory(false)}
         >
           Cancel
         </button>
+      </th>
+      <th>
+        <button className="btn btn-accent ">Save</button>
       </th>
     </tr>
   );
