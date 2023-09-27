69137b63f40c9d726b4b883dc512b22f9dce3bb0
jbettmann <jordan.bettmann@gmail.com>
2023-09-27 16:36:39 -0600

    added disabled

--- src/components/Buttons/SaveButton.tsx
+++ src/components/Buttons/SaveButton.tsx
@@ -2,18 +2,19 @@ import React, { MouseEventHandler } from "react";
 
 type Props = {
   isLoading: boolean;
-  onClick?: (e: Event) => void;
+  onClick?: (e: any) => void;
   className?: string;
+  disabled?: boolean | undefined;
 };
 
-const SaveButton = ({ isLoading, onClick, className }: Props) => {
+const SaveButton = ({ isLoading, onClick, className, disabled }: Props) => {
   return (
-    <button className={className ? className : "create-btn"} onClick={onClick}>
-      {isLoading ? (
-        <span className="loading loading-spinner text-accent"></span>
-      ) : (
-        "Save"
-      )}
+    <button
+      disabled={disabled || undefined}
+      className={className ? className : "create-btn"}
+      onClick={onClick}
+    >
+      {isLoading ? <span className="loading loading-spinner"></span> : "Save"}
     </button>
   );
 };
