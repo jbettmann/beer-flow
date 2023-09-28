505cac8b8aceaf16ce466bfe5094d14ad8f2de11
jbettmann <jordan.bettmann@gmail.com>
2023-09-28 16:46:26 -0600

    updated for small screen

--- src/components/Invite/Invite.tsx
+++ src/components/Invite/Invite.tsx
@@ -10,33 +10,31 @@ type Props = {
 
 const Invite = ({ invitee, handleChange, className, error }: Props) => {
   return (
-    <div className="flex justify-center items-center mb-6 ">
-      <div className="text-left flex relative">
-        <div className="flex-1">
-          <label htmlFor="email" className="label-text m-5 text-background">
-            Email Address
-          </label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Email"
-            className={`${className} form__input w-full `}
-            value={invitee.email}
-            onChange={(e) => handleChange("email", e.target.value)}
-          />
-          {error && <p className="error absolute right-12 top-9 ">{error}</p>}
-        </div>
-        <div className="flex-none px-3 text-center ">
-          <label className="cursor-pointer label label-text text-background">
-            Admin
-          </label>
-          <input
-            type="checkbox"
-            className="checkbox checkbox-accent"
-            onChange={(e) => handleChange("isAdmin", e.target.checked)}
-            checked={invitee.isAdmin}
-          />
-        </div>
+    <div className="flex flex-wrap justify-center items-center mb-6 text-left  relative">
+      <div className="flex-1">
+        <label htmlFor="email" className="label-text m-5 text-background">
+          Email Address
+        </label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          className={`${className} form__input w-full  `}
+          value={invitee.email}
+          onChange={(e) => handleChange("email", e.target.value)}
+        />
+        {error && <p className="error absolute right-12 top-9 ">{error}</p>}
+      </div>
+      <div className="flex-none px-1 xs:px-3 text-center ">
+        <label className="cursor-pointer label label-text text-background">
+          Admin
+        </label>
+        <input
+          type="checkbox"
+          className="checkbox checkbox-accent"
+          onChange={(e) => handleChange("isAdmin", e.target.checked)}
+          checked={invitee.isAdmin}
+        />
       </div>
     </div>
   );
