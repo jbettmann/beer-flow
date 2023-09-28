15268273f0b9503daa4c1bda6cda7bd012da2933
jbettmann <jordan.bettmann@gmail.com>
2023-09-27 22:17:29 -0600

    added common layout heading

--- src/components/StaffManagement/StaffContainer.tsx
+++ src/components/StaffManagement/StaffContainer.tsx
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 import StaffDashboard from "./StaffDashboard";
 import StaffTable from "./StaffTable";
 import Link from "next/link";
-import { UserPlus } from "lucide-react";
+import { Plus, UserPlus } from "lucide-react";
 import BottomDrawer from "../Drawers/BottomDrawer";
 import MultipleInvites from "../Invite/MultipuleInvites";
 import { Users } from "@/app/types/users";
@@ -21,16 +21,31 @@ const StaffContainer = (props: Props) => {
     selectedBrewery?.staff && (
       <>
         <div className="flex justify-between md:p-5">
-          <div className="flex flex-col w-fit">
-            <h3 className="text-left">{selectedBrewery.companyName} Staff</h3>
+          <div className="flex flex-col w-fit mx-auto lg:m-0 lg:my-auto ">
+            <h3 className="text-center lg:text-left">
+              {selectedBrewery.companyName} Staff
+            </h3>
             <div className="text-sm badge badge-ghost opacity-50 mt-2">
               Owner {(selectedBrewery.owner as Users).fullName || ""}
             </div>
           </div>
-          <button onClick={() => setIsOpen(true)} className="btn btn-accent">
-            Invite <UserPlus size={20} />
+          <div className="hidden lg:block">
+            <button onClick={() => setIsOpen(true)} className="create-btn">
+              Invite <UserPlus size={20} />
+            </button>
+          </div>
+        </div>
+        {/* Small Screen New Category Button */}
+        <div className="fixed right-5 bottom-20 p-1 z-[2] lg:hidden ">
+          <button
+            onClick={() => setIsOpen(true)}
+            className="btn btn-circle btn-white create-btn !btn-lg"
+          >
+            <Plus size={28} />
           </button>
         </div>
+
+        {/* Layout */}
         <div className="flex flex-col w-full  md:p-5">
           <StaffDashboard
             setViewFilter={setViewFilter}
