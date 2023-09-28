613e1f69c4f02c664a9630d467ed2e98a3aa91d0
jbettmann <jordan.bettmann@gmail.com>
2023-09-28 16:47:04 -0600

    layout for edit

--- src/components/StaffManagement/StaffMemberCard.tsx
+++ src/components/StaffManagement/StaffMemberCard.tsx
@@ -10,6 +10,8 @@ import SaveButton from "../Buttons/SaveButton";
 
 import deleteStaffMember from "@/lib/DELETE/deleteStaffMemeber";
 import TrashCanIcon from "../Buttons/TrashCanIcon";
+import AlertDialog from "../Alerts/AlertDialog";
+import { useToast } from "@/context/toast";
 
 type Props = {
   staff: Users;
@@ -35,7 +37,11 @@ const StaffMemberCard = ({
   const [isChecked, setIsChecked] = useState(checkedStaffIds.has(staff._id));
   const [isAdmin, setIsAdmin] = useState<boolean>(admin);
   const [initialIsAdmin, setInitialIsAdmin] = useState(admin); // compare to isAdmin for change
+  const [hasChanged, setHasChanged] = useState<boolean>(false);
+  const [deleteStaffMemberAlert, setDeleteStaffMemberAlert] =
+    useState<boolean>(false);
 
+  const { addToast } = useToast();
   const rowRef = useRef(null);
   // update if user is admin or not of Brewery
   const handleAdminChange = async () => {
@@ -53,11 +59,12 @@ const StaffMemberCard = ({
           });
 
           console.log(updatedAdmin.message);
-          alert(updatedAdmin.message);
+          addToast(updatedAdmin.message, "success");
           setSelectedBrewery(updatedAdmin.brewery);
 
           // After successfully updating, set the initialIsAdmin to the current isAdmin value
           setInitialIsAdmin(isAdmin);
+          setHasChanged(false);
         }
       } catch (error) {
         console.log(error);
@@ -80,16 +87,22 @@ const StaffMemberCard = ({
         userId: staff._id,
         accessToken: session?.user.accessToken,
       });
-      console.log(result.message, result.updatedBrewery);
-      alert(result.message);
+
+      addToast(result.message, "success");
       setSelectedBrewery(result.updatedBrewery);
       // Display success message from the result
     } catch (err: any) {
       console.error(err);
-      alert(err.message); // Displaying the error message as an alert on the client side
+      addToast(err.message, "error"); // Displaying the error message as an alert on the client side
     } finally {
       setIsLoading(false);
     }
+  };
+
+  const handleCancelEdit = () => {
+    setIsEdit(false);
+    setIsAdmin(initialIsAdmin);
+    setHasChanged(false);
   };
 
   const useOutsideClick = (
@@ -113,7 +126,7 @@ const StaffMemberCard = ({
   //  close edit mode when clicking outside of row
   useOutsideClick(rowRef, () => {
     if (isEdit) {
-      setIsEdit(false);
+      handleCancelEdit();
     }
   });
 
@@ -121,6 +134,8 @@ const StaffMemberCard = ({
   useEffect(() => {
     setIsChecked(checkedStaffIds.has(staff._id));
   }, [checkedStaffIds]);
+
+ 
 
   return (
     <div
@@ -130,87 +145,117 @@ const StaffMemberCard = ({
         isChecked ? "table-row__checked" : ""
       }`}
       onClick={() => {
+        if (isEdit) return;
         handleCheckboxChange(staff._id);
       }}
     >
-      <div>
-        <div className="flex items-center space-x-6">
-          <div className="avatar">
-            <div className="mask mask-squircle object-cover h-20">
-              <Image
-                src={staff.image || ""}
-                alt={`profile picture of ${staff.fullName}`}
-                className=""
-                width={50}
-                height={50}
-              />
-            </div>
-          </div>
-          <div className="space-y-1">
-            <div className="font-bold">{staff.fullName}</div>
-            <div className="text-sm opacity-50">{staff.email}</div>
-            <span className="badge font-normal">{role}</span>
+      <AlertDialog
+        title="You sure?"
+        message={`Are you sure you want to remove ${staff.fullName} from staff?`}
+        isOpen={deleteStaffMemberAlert}
+        onClose={() => setDeleteStaffMemberAlert(false)}
+        onConfirm={handleDeleteStaff}
+        confirmButtonText="Remove"
+      />
+
+      <div
+        className={`flex space-x-6 transition-all  ${
+          isEdit ? "items-start" : ""
+        }`}
+      >
+        <div className="avatar">
+          <div className="mask mask-squircle object-cover h-20">
+            <Image
+              src={staff.image || ""}
+              alt={`profile picture of ${staff.fullName}`}
+              className=""
+              width={50}
+              height={50}
+            />
           </div>
         </div>
-      </div>
-
-      <div className="z-[1]">
-        {isEdit && (
-          <div className="form-control flex flex-col justify-center">
-            <div className="label-text ml-1">Admin</div>
-
-            <label className="cursor-pointer label w-fit">
-              <input
-                type="checkbox"
-                className="toggle toggle-accent"
-                onClick={(e) => {
-                  e.stopPropagation(), setIsAdmin(!isAdmin);
-                }}
-                checked={isAdmin}
-              />
-            </label>
+        <div className={`space-y-1 transition-all  ${isEdit ? "pb-14" : ""}`}>
+          <div className="font-bold">{staff.fullName}</div>
+          <div className="text-sm opacity-50">{staff.email}</div>
+          <div className="flex items-center">
+            <span
+              className={`${
+                isEdit
+                  ? "flex justify-center items-center gap-2 z-[1]"
+                  : "badge font-normal"
+              }`}
+            >
+              {isEdit ? (
+                <>
+                  <label className="cursor-pointer label w-fit">
+                    {isAdmin ? "Admin" : "Crew"}
+                  </label>
+                  <input
+                    type="checkbox"
+                    className="toggle toggle-accent"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setIsAdmin(!isAdmin);
+                      setHasChanged(true);
+                    }}
+                    checked={isAdmin}
+                  />
+                </>
+              ) : (
+                role
+              )}
+            </span>
+            {isChecked && !isEdit && role !== "Owner" && (
+              <span>
+                <button
+                  className={`btn btn-sm btn-circle btn-ghost`}
+                  onClick={(e) => {
+                    e.stopPropagation(), setIsEdit(true);
+                  }}
+                >
+                  <PencilLine size={24} strokeWidth={1} />
+                </button>
+              </span>
+            )}
           </div>
-        )}
-      </div>
-      {/* If owner, skull */}
-      <div className="absolute top-0 right-0 z-[1]">
-        {isChecked && role === "Owner" ? (
-          <div className="btn btn-circle btn-ghost">
-            <SkullIcon size={26} strokeWidth={1} />
-          </div>
-        ) : (
-          // If not owner, edit and delete
-          <>
-            {isChecked && isEdit ? (
+          {isEdit && (
+            <div className="flex gap-3 absolute bottom-4 right-0 px-4 ">
+              <button
+                className="btn btn-ghost text-primary " // Made the button more visible
+                onClick={handleCancelEdit}
+              >
+                Cancel
+              </button>
               <SaveButton
                 onClick={(e) => {
                   e.stopPropagation(), handleAdminChange();
                 }}
                 isLoading={isLoading}
+                disabled={!hasChanged}
               />
-            ) : (
-              <button
-                className={`btn btn-circle btn-ghost`}
-                onClick={(e) => {
-                  e.stopPropagation(), setIsEdit(true);
-                }}
-              >
-                <PencilLine size={24} strokeWidth={1} />
-              </button>
-            )}
-            {isChecked && (
-              <>
-                <div className="divider divider-horizontal"></div>
+            </div>
+          )}
+        </div>
+      </div>
 
-                <TrashCanIcon
-                  onClick={(e) => {
-                    e.stopPropagation(), handleDeleteStaff();
-                  }}
-                  isLoading={isLoading}
-                />
-              </>
-            )}
-          </>
+      {/* If owner, skull */}
+      <div className="absolute top-0 right-0 z-[1] flex flex-col justify-between">
+        {isChecked && role === "Owner" ? (
+          <div className="btn btn-circle btn-ghost">
+            <SkullIcon size={26} strokeWidth={1} />
+          </div>
+        ) : (
+          isChecked && (
+            <>
+              <TrashCanIcon
+                onClick={(e) => {
+                  e.stopPropagation(), setDeleteStaffMemberAlert(true);
+                }}
+                isLoading={isLoading}
+                className="btn btn-circle btn-ghost"
+              />
+            </>
+          )
         )}
       </div>
     </div>
