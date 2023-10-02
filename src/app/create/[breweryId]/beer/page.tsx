138120ec43c4a91c0f878dcb1814259fb915653d
jbettmann <jordan.bettmann@gmail.com>
2023-10-02 15:11:35 -0600

    added width styling for screen size

--- src/app/create/[breweryId]/beer/page.tsx
+++ src/app/create/[breweryId]/beer/page.tsx
@@ -14,7 +14,8 @@ export default async function CreateBeerPage({
 
   return (
     // SingleBeerPageContainer Only on [beerId] page to set selectedBeers State *
-
-    <CreateBeerForm />
+    <section className="w-full sm:w-2/3 lg:w-1/2 mx-auto h-full ">
+      <CreateBeerForm />
+    </section>
   );
 }
