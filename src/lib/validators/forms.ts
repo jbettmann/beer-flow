8ebdd6e5d3dbc9c27864d68076100666ebeade74
jbettmann <jordan.bettmann@gmail.com>
2023-10-03 15:09:22 -0600

    fixed errors

--- src/lib/validators/forms.ts
+++ src/lib/validators/forms.ts
@@ -1,9 +1,10 @@
 import { ErrorValues, FormValues } from "@/components/CreateBeerForm/types";
-import exp from "constants";
 
 // Utility function to validate form fields
 const validateFields = (values: FormValues) => {
   const errors: ErrorValues = {};
+
+  console.log("values", values);
 
   // validate name
 
@@ -17,22 +18,17 @@ const validateFields = (values: FormValues) => {
   }
 
   // validate abv
-  if (!/^(\d+(\.\d{1,2})?)$/.test(values.abv)) {
+  if (!/^(\d+(\.\d{1,2})?)$/.test(values.abv as string)) {
     errors.abv = "ABV must be a number.";
   }
 
-  if (!/^(\d+(\.\d{1,2})?)$/.test(values.ibu)) {
+  if (!/^(\d+(\.\d{1,2})?)$/.test(values.ibu as string)) {
     errors.ibu = "IBU must be a number.";
   }
 
   // validate category
   if (values.category.length === 0) {
     errors.category = "A category is required.";
-  }
-
-  // validate image
-  if (!values.image) {
-    errors.image = "Image is required.";
   }
 
   return errors;
