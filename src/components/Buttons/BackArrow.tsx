6ad2e88e7c782f96569d088b901612f705efd8a6
jbettmann <jordan.bettmann@gmail.com>
2023-10-04 16:10:27 -0600

    Create BackArrow.tsx

---
+++ src/components/Buttons/BackArrow.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useBreweryContext } from "@/context/brewery-beer";
+import { MoveLeft } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+type Props = {};
+
+const BackArrow = (props: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const { selectedBrewery } = useBreweryContext();
+
+  const [homepage, setHomepage] = useState<string | null>(null);
+  const handleBack = () => {
+    router.back();
+  };
+
+  useEffect(() => {
+    if (selectedBrewery) {
+      setHomepage(`/breweries/${selectedBrewery?._id}`);
+    }
+  }, [selectedBrewery?._id]);
+
+  return (
+    homepage && (
+      <h2
+        onClick={handleBack}
+        className={`${
+          homepage === pathname ? "hidden" : "block my-2 sm:my-4 lg:hidden"
+        }`}
+      >
+        <MoveLeft size={30} />
+      </h2>
+    )
+  );
+};
+
+export default BackArrow;
