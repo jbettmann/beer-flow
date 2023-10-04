75bfdd1600d2a1027774a63407072666c0d413cf
jbettmann <jordan.bettmann@gmail.com>
2023-10-04 16:11:42 -0600

    added type

--- src/components/Buttons/SaveButton.tsx
+++ src/components/Buttons/SaveButton.tsx
@@ -1,25 +1,30 @@
 import React, { MouseEventHandler } from "react";
 
 type Props = {
+  title?: string;
   isLoading: boolean;
   onClick?: (e: any) => void;
   className?: string;
   disabled?: boolean | undefined;
+  type?: "button" | "submit" | "reset" | undefined;
 };
 
 const SaveButton = ({
+  title = "Save",
   isLoading,
   onClick,
   className = "",
   disabled,
+  type = "button",
 }: Props) => {
   return (
     <button
       disabled={disabled || undefined}
       className={`create-btn ${className}`}
       onClick={onClick}
+      type={type}
     >
-      {isLoading ? <span className="loading loading-spinner"></span> : "Save"}
+      {isLoading ? <span className="loading loading-spinner"></span> : title}
     </button>
   );
 };
