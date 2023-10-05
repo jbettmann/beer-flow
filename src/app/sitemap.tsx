0c68b96d85fc9062057a3622f3cdbe1133203c25
jbettmann <jordan.bettmann@gmail.com>
2023-10-05 10:37:47 -0600

    Update sitemap.tsx

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -1,18 +1,18 @@
 import { Users } from "@/app/types/users";
 
 export default async function sitemap() {
-  const res = await fetch("https://beer-bible-api.vercel.app/users");
-  const allUsers = (await res.json()) as Users[];
+  // const res = await fetch("https://beer-bible-api.vercel.app/users");
+  // const allUsers = (await res.json()) as Users[];
 
-  const users = allUsers.map((brewery) => ({
-    url: `http://localhost:3000/brewery/${brewery._id}`,
-    lastModified: new Date().toISOString(),
-  }));
+  // const users = allUsers.map((brewery) => ({
+  //   url: `http://localhost:3000/brewery/${brewery._id}`,
+  //   lastModified: new Date().toISOString(),
+  // }));
 
-  const routes = ["", "about", "beers", "login"].map((route) => ({
+  const routes = ["", "breweries", "create"].map((route) => ({
     url: `http://localhost:3000/${route}`,
     lastModified: new Date().toISOString(),
   }));
 
-  return [...routes, ...users];
+  return [...routes];
 }
