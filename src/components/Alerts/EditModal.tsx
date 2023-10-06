48245d560b190bb54387f389a82d70089ccd8c9c
jbettmann <jordan.bettmann@gmail.com>
2023-10-06 16:59:49 -0600

    Create EditModal.tsx

---
+++ src/components/Alerts/EditModal.tsx
@@ -0,0 +1,42 @@
+import React, { FC, useEffect, useRef } from "react";
+
+type EditModalProps = {
+  title?: string;
+  children: React.ReactNode;
+  isOpen: boolean;
+
+  confirmButtonText?: string;
+  cancelButtonText?: string;
+};
+
+const EditModal: FC<EditModalProps> = ({
+  title = "",
+  children,
+  isOpen,
+
+  confirmButtonText = "Save",
+  cancelButtonText = "Cancel",
+}) => {
+  const modalRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (isOpen && modalRef.current) {
+      modalRef.current.showModal();
+    } else if (modalRef.current) {
+      modalRef.current.close();
+    }
+  }, [isOpen]);
+
+  return (
+    <dialog ref={modalRef} id="alert_modal" className="modal modal-middle ">
+      <div className="modal-box p-5 bg-primary ">
+        <div className="flex text-center flex-col alert alert-modal">
+          <h4>{title}</h4>
+          <div className="w-full">{children}</div>
+        </div>
+      </div>
+    </dialog>
+  );
+};
+
+export default EditModal;
