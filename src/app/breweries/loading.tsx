fad85bc7d9e72e182ce7e438480b703ca6e01e63
jbettmann <jordan.bettmann@gmail.com>
2023-10-11 16:46:28 -0600

    Create loading.tsx

---
+++ src/app/breweries/loading.tsx
@@ -0,0 +1,10 @@
+import BreweriesPageLS from "@/components/LoadingSkeleton/BreweriesPageLS";
+import React from "react";
+
+type Props = {};
+
+const loading = (props: Props) => {
+  return <BreweriesPageLS />;
+};
+
+export default loading;
