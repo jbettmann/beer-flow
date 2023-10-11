8064f19d9fa4e0ec0aa3436d777a51c0a107232d
jbettmann <jordan.bettmann@gmail.com>
2023-10-11 16:46:51 -0600

    added suspense and created Breweries Component

--- src/app/breweries/page.tsx
+++ src/app/breweries/page.tsx
@@ -1,9 +1,9 @@
 // import { PageProps } from "../../../.next/types/app/layout";
-import SetBreweryIdStorage from "@/components/Buttons/SetBreweryIdStorage";
 import getBreweries from "@/lib/getBreweries";
-import { Plus } from "lucide-react";
-import Link from "next/link";
 import { Brewery } from "../types/brewery";
+import Breweries from "@/components/Breweries/Breweries";
+import { Suspense } from "react";
+import BreweriesPageLS from "@/components/LoadingSkeleton/BreweriesPageLS";
 
 const BreweriesPage = async () => {
   const breweryData: Promise<Brewery[]> = getBreweries();
@@ -11,44 +11,9 @@ const BreweriesPage = async () => {
 
   return (
     <section className="w-full h-screen py-3 sm:p-8">
-      <div className="flex flex-col justify-center  mx-auto text-center gap-6">
-        <div className="flex justify-between md:p-5 ">
-          <div className="flex flex-col w-fit mx-auto lg:m-0 lg:my-auto ">
-            <h3 className="text-center lg:text-left">Breweries</h3>
-          </div>
-          <div className="hidden lg:flex justify-center items-center gap-2">
-            <Link href={"/create/brewery"} className="create-btn  ">
-              + Brewery
-            </Link>
-          </div>
-        </div>
-        <div className="flex flex-col justify-center w-[80%] items-center mx-auto gap-8">
-          {breweries.length > 0 &&
-            breweries.map((brewery) => {
-              return (
-                <>
-                  <div
-                    key={brewery._id}
-                    className="category-card w-full md:w-1/2 rounded-xl p-5 sm:p-6"
-                  >
-                    <SetBreweryIdStorage
-                      brewery={brewery}
-                      href={`/breweries/${brewery._id}`}
-                    />
-                  </div>
-                </>
-              );
-            })}
-        </div>
-        <div className="fixed right-5 bottom-20 p-1 z-[2] lg:hidden ">
-          <Link
-            href={"/create/brewery"}
-            className="btn btn-circle btn-white create-btn !btn-lg"
-          >
-            <Plus size={28} />
-          </Link>
-        </div>
-      </div>
+      <Suspense fallback={<BreweriesPageLS />}>
+        <Breweries breweries={breweries} />
+      </Suspense>
     </section>
   );
 };
