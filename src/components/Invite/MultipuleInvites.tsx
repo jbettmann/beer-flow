935363b5d86862abfa95e255fff1452c702f5156
jbettmann <jordan.bettmann@gmail.com>
2023-10-12 17:47:39 -0600

    Update MultipuleInvites.tsx

--- src/components/Invite/MultipuleInvites.tsx
+++ src/components/Invite/MultipuleInvites.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Invite from "./Invite";
 import { sendInvite } from "@/lib/POST/sendInvite";
 import { validateEmail } from "@/lib/validators/email";
@@ -17,11 +17,12 @@ type pageProps = {
 
 const MultipleInvites = ({ breweryId, setIsOpen }: pageProps) => {
   const { data: session } = useSession();
-  const router = useRouter();
   const { addToast } = useToast();
   const [invitees, setInvitees] = useState([
     { email: "", isAdmin: false, error: "" },
   ]);
+  const [hasStartedInvite, setHasStartedInvite] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const { selectedBrewery } = useBreweryContext();
 
   const addInvitee = () => {
@@ -34,6 +35,7 @@ const MultipleInvites = ({ breweryId, setIsOpen }: pageProps) => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsLoading(true);
 
     // validate email addresses and set errors if necessary
     const newInvitees = invitees.map((invitee) => {
@@ -43,6 +45,7 @@ const MultipleInvites = ({ breweryId, setIsOpen }: pageProps) => {
           error: "Invalid email address.",
         };
       }
+
       return invitee;
     });
 
@@ -83,7 +86,6 @@ const MultipleInvites = ({ breweryId, setIsOpen }: pageProps) => {
           );
 
           setInvitees([{ email: "", isAdmin: false, error: "" }]);
-          router.back();
         }
         if (failedEmails.length) {
           addToast(
@@ -97,6 +99,7 @@ const MultipleInvites = ({ breweryId, setIsOpen }: pageProps) => {
       addToast(error.message, "error");
       console.error("Error sending invites:", error);
     }
+    setIsLoading(false);
   };
 
   const onClose = () => {
@@ -112,82 +115,117 @@ const MultipleInvites = ({ breweryId, setIsOpen }: pageProps) => {
     const newInvitees: any = [...invitees];
     newInvitees[index][field] = value;
 
-    // If there's an error and the field is email, clear the error
-    if (field === "email" && newInvitees[index].error) {
-      newInvitees[index].error = "";
+    if (field === "email") {
+      if (value === "") {
+        newInvitees[index].error = "Email address required to send invite";
+        setHasStartedInvite(false);
+      } else {
+        newInvitees[index].error = "";
+        setHasStartedInvite(true);
+      }
     }
+
     setInvitees(newInvitees);
   };
 
   return (
-    <>
-      <div className="flex w-full justify-between items-center">
-        <button onClick={onClose} className="btn btn-ghost m-2 ">
-          <X size={24} />
-        </button>
-        <h3>Invite</h3>
-        <button
-          onClick={onClose}
-          className="btn btn-ghost btn-disabled invisible m-2 "
-        >
-          <X size={24} />
-        </button>
-      </div>
-
+    <div>
       {selectedBrewery && (
         <form
           onSubmit={handleSubmit}
-          className="form flex flex-col w-full lg:w-1/2 mt-8 rounded-lg mx-auto"
+          className="py-4 flex flex-col justify-between mx-auto rounded-lg text-white lg:p-0"
         >
-          <div className="flex flex-col items-center">
-            <div className="flex justify-between">
-              <div className="flex flex-col ">
-                {invitees.map((invitee, index) => (
-                  <div
-                    className={`p-3 flex flex-col xxs:flex-row xxs:items-center relative animate-appear `}
-                    key={index}
-                  >
-                    {index !== 0 ? (
-                      <button
-                        type="button"
-                        className="btn-link link-error no-underline absolute bottom-2 ml-5 xxs:ml-0 xxs:mt-2 xxs:relative  xxs:btn xxs:btn-outline xxs:text-error xxs:rounded-full xxs:w-6 xxs:h-9 xxs:min-w-0 xxs:min-h-fit mr-3"
-                        onClick={() => removeInvitee(index)}
-                      >
-                        <span className="hidden xxs:flex ">-</span>
-                        <span className="xxs:hidden text-sm">Remove</span>
-                      </button>
-                    ) : (
-                      <div className="btn btn-outline btn-ghost rounded-full w-5 h-9 min-w-5 min-h-fit mr-3"></div>
-                    )}
-                    <Invite
-                      invitee={invitee}
-                      className={""}
-                      handleChange={(field, value) =>
-                        handleChange(index, field, value)
-                      }
-                      error={invitee.error}
-                    />
-                  </div>
-                ))}
-                <div className="flex justify-end  items-center pr-6 gap-2">
-                  <button
-                    type="button"
-                    className="create-btn inverse"
-                    onClick={addInvitee}
-                  >
-                    <Plus size={18} strokeWidth={2} />
-                  </button>
+          <div className="flex w-full justify-between items-center p-3 pt-0 lg:hidden">
+            <button onClick={onClose} className="btn btn-ghost " type="button">
+              <X size={24} />
+            </button>
+            <h3>Invite</h3>
+            <button
+              aria-label="Send Invites"
+              className=" text-background px-4 "
+              type="submit"
+              disabled={
+                !hasStartedInvite ||
+                invitees.some((invitee) => !invitee.email || invitee.error)
+              }
+            >
+              {isLoading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                <Send size={25} />
+              )}
+            </button>
+          </div>
+          <div className="flex justify-between">
+            <div className="flex flex-col ">
+              {invitees.map((invitee, index) => (
+                <div
+                  className={`p-3 flex flex-col xxs:flex-row xxs:items-center relative animate-appear `}
+                  key={index}
+                >
+                  {index !== 0 ? (
+                    <button
+                      type="button"
+                      className="btn-link link-error no-underline absolute bottom-2 ml-5 xxs:ml-0 xxs:mt-3 xxs:relative  xxs:btn xxs:btn-outline xxs:text-error xxs:rounded-full xxs:w-6 xxs:h-9 xxs:min-w-0 xxs:min-h-fit mr-3"
+                      onClick={() => removeInvitee(index)}
+                    >
+                      <span className="hidden xxs:flex ">-</span>
+                      <span className="xxs:hidden text-sm">Remove</span>
+                    </button>
+                  ) : (
+                    <div className=" w-9 h-9 min-w-5 min-h-fit mr-3"></div>
+                  )}
+                  <Invite
+                    invitee={invitee}
+                    className={""}
+                    handleChange={(field, value) =>
+                      handleChange(index, field, value)
+                    }
+                    error={invitee.error}
+                  />
                 </div>
+              ))}
+              <div className="flex justify-end  items-center pr-6 gap-2">
+                <button
+                  type="button"
+                  className="create-btn inverse"
+                  onClick={addInvitee}
+                >
+                  <Plus size={18} strokeWidth={2} />
+                </button>
               </div>
             </div>
+          </div>
 
-            <button className="create-btn inverse mx-auto mt-10" type="submit">
-              <Send size={22} /> Invite
+          <div className=" hidden lg:flex justify-between items-center w-3/4 p-3 mt-6 pb-0 mx-auto">
+            <button
+              className="btn border-none bg-transparent hover:bg-background hover:text-primary text-background"
+              onClick={onClose}
+              type="button"
+            >
+              Cancel
+            </button>
+
+            <button
+              className="create-btn inverse"
+              type="submit"
+              disabled={
+                !hasStartedInvite ||
+                invitees.some((invitee) => !invitee.email || invitee.error)
+              }
+            >
+              {isLoading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                <>
+                  <Send size={25} /> Invite
+                </>
+              )}
             </button>
           </div>
         </form>
       )}
-    </>
+    </div>
   );
 };
 
