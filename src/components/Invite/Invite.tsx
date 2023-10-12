cbf3b886a7d803f0f1b256667d4e36b2c599f4f5
jbettmann <jordan.bettmann@gmail.com>
2023-10-12 17:47:36 -0600

    updated errors

--- src/components/Invite/Invite.tsx
+++ src/components/Invite/Invite.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { validateEmail } from "@/lib/validators/email";
 import React from "react";
 
 type Props = {
@@ -10,10 +11,10 @@ type Props = {
 
 const Invite = ({ invitee, handleChange, className, error }: Props) => {
   return (
-    <div className="flex flex-wrap justify-center items-center mb-6 text-left  relative">
-      <div className="flex-1">
+    <div className="flex flex-wrap justify-center items-center mb-4 text-left ">
+      <div className="flex-1 relative">
         <label htmlFor="email" className="label-text m-5 text-background">
-          Email Address
+          Email Address*
         </label>
         <input
           type="email"
@@ -21,9 +22,13 @@ const Invite = ({ invitee, handleChange, className, error }: Props) => {
           placeholder="Email"
           className={`${className} form__input w-full  `}
           value={invitee.email}
-          onChange={(e) => handleChange("email", e.target.value)}
+          onChange={(e) => {
+            handleChange("email", e.target.value);
+          }}
         />
-        {error && <p className="error absolute right-12 top-9 ">{error}</p>}
+        {error && (
+          <p className="error m-0 absolute ml-4 mt-1 !text-xs">{error}</p>
+        )}
       </div>
       <div className="flex-none px-1 xxs:px-3 text-center ">
         <label className="cursor-pointer label label-text text-background">
