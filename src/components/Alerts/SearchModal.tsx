b2920791734bf519063621d69820cbb50779a4c2
jbettmann <jordan.bettmann@gmail.com>
2023-10-20 15:37:40 -0600

    Create SearchModal.tsx

---
+++ src/components/Alerts/SearchModal.tsx
@@ -0,0 +1,37 @@
+import React, { FC, useEffect, useRef } from "react";
+
+type SearchModalProps = {
+  title?: string;
+  children: React.ReactNode;
+  isOpen: boolean;
+};
+
+const SearchModal: FC<SearchModalProps> = ({
+  title = "",
+  children,
+  isOpen,
+}) => {
+  const modalRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (isOpen && modalRef.current) {
+      modalRef.current.showModal();
+    } else if (modalRef.current) {
+      modalRef.current.close();
+    }
+  }, [isOpen]);
+
+  return (
+    <dialog
+      ref={modalRef}
+      id="alert_modal"
+      className="modal modal-middle bg-white/30  "
+    >
+      <div className="modal-box bg-fourth-color absolute top-1/4 h-fit max-h-[32rem] transition-all ease-in-out">
+        <div className="w-full p-0">{children}</div>
+      </div>
+    </dialog>
+  );
+};
+
+export default SearchModal;
