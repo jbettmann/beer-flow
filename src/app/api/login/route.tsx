2d03bd70b2c61a0d3c87f0aa518fca38379d43f8
jbettmann <jordan.bettmann@gmail.com>
2023-10-24 14:26:31 -0600

    Revert "Delete route.tsx"
    This reverts commit 177ee31b18a1743beea728f69e0594dda70aaffe.

---
+++ src/app/api/login/route.tsx
@@ -0,0 +1,42 @@
+import db from "@/lib/db";
+import { signJwtAccessToken } from "@/lib/jwt";
+
+import * as bcyrpt from "bcrypt";
+
+interface RequestBody {
+  username: string;
+  password: string;
+}
+
+export async function POST(req: Request, res: Response) {
+  try {
+    const body: RequestBody = await req.json();
+
+    const client = await db;
+    const collection = client.db().collection("users");
+
+    const user = await collection.findOne({ username: body.username });
+
+    if (user) {
+      const isPasswordValid = await bcyrpt.compare(
+        body.password,
+        user.password
+      );
+
+      if (isPasswordValid) {
+        const { password, ...userWithoutPassword } = user;
+        const accessToken = signJwtAccessToken(userWithoutPassword);
+        const result = {
+          ...userWithoutPassword,
+          accessToken,
+        };
+        return new Response(JSON.stringify(result));
+      }
+    }
+
+    return new Response(JSON.stringify(null));
+  } catch (error) {
+    console.error("Error handling POST request:", error);
+    return new Error(JSON.stringify(error));
+  }
+}
