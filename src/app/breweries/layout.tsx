3b83ff153a67350e4b2f040ffe770784d0678865
jbettmann <jordan.bettmann@gmail.com>
2023-10-26 15:58:34 -0600

    Update layout.tsx

--- src/app/breweries/layout.tsx
+++ src/app/breweries/layout.tsx
@@ -4,7 +4,7 @@ export default async function BreweriesLayout(props: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="h-full py-3 lg:py-8 lg:pr-12">
+    <div className="h-full py-3 mt-8 lg:py-8 lg:pr-12">
       {/* @ts-expect-error Server component */}
       <BackArrow />
       {props.children}
