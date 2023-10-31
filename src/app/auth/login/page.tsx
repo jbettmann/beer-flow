3ad3af136f7e8449694a8c67c38d0c068d56aa54
jbettmann <jordan.bettmann@gmail.com>
2023-10-31 13:42:15 -0600

    updated login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -77,110 +77,126 @@ const LoginPage = () => {
   }
 
   return (
-    <div className="h-screen w-full flex flex-col justify-center items-center">
-      <h1>Brett</h1>
+    <div className="h-screen w-full flex flex-col pt-12 items-center">
+      <h1 className="text-primary font-medium absolute top-0 left-5 p-4">
+        Brett
+      </h1>
+      <div className="relative flex flex-col justify-center items-center">
+        <div className="w-screen md:h-96 bg-accent/30 text-primary p-8 relative">
+          <h2 className="font-semibold leading-[1.25] text-2xl md:text-5xl text-center md:w-2/3 mx-auto">
+            Keep your brewery up-to-date.
+          </h2>
+          <p className="font-thin md:w-1/2 text-sm md:text-base leading-7 mx-auto text-center">
+            Brett is the vital bridge between your front and back of house,
+            ensuring accurate and detailed information, in real-time, that is
+            readily available.
+          </p>
+        </div>
 
-      <div className="w-full md:w-1/2  mx-auto justify-center items-center shadow-lg rounded-lg p-12 mt-6 bg-white ">
-        <h3 className="font-normal">Log in to your account</h3>
-        <div className="pt-8">
-          <button
-            onClick={(e) => onSignIn(e, "google")}
-            disabled={isGoogleLoading}
-            className=" flex justify-center items-center w-full p-4 rounded-md bg-white border border-primary hover:shadow-xl transition-all ease-in-out"
-          >
-            {isGoogleLoading ? (
-              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-            ) : (
-              <svg
-                className="mr-2 h-6 w-6"
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fab"
-                data-icon="github"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
+        <div className="w-[90%] md:w-1/2  mx-auto justify-center items-center shadow-lg rounded-lg p-12 mt-6 bg-white absolute top-[77%] md:top-1/2 md:left-1/4">
+          <h3 className="font-normal">Log in to your account</h3>
+          <div className="pt-8">
+            <button
+              onClick={(e) => onSignIn(e, "google")}
+              disabled={isGoogleLoading}
+              className=" flex justify-center items-center w-full p-4 rounded-md bg-white border border-primary hover:shadow-xl transition-all ease-in-out"
+            >
+              {isGoogleLoading ? (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              ) : (
+                <svg
+                  className="mr-2 h-6 w-6"
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fab"
+                  data-icon="github"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+                    fill="#4285F4"
+                  />
+                  <path
+                    d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+                    fill="#34A853"
+                  />
+                  <path
+                    d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+                    fill="#FBBC05"
+                  />
+                  <path
+                    d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+                    fill="#EA4335"
+                  />
+                  <path d="M1 1h22v22H1z" fill="none" />
+                </svg>
+              )}
+              Google
+            </button>
+          </div>
+          <div className="divider my-6">
+            <h5 className="my-3">OR</h5>
+          </div>
+          <form>
+            <div className="flex flex-col">
+              <label
+                className="beer-card__label-text !text-primary"
+                htmlFor="email"
               >
-                <path
-                  d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
-                  fill="#4285F4"
-                />
-                <path
-                  d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
-                  fill="#34A853"
-                />
-                <path
-                  d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
-                  fill="#FBBC05"
-                />
-                <path
-                  d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
-                  fill="#EA4335"
-                />
-                <path d="M1 1h22v22H1z" fill="none" />
-              </svg>
+                Email
+              </label>
+              <input
+                className="form-input__create-account"
+                type="email"
+                placeholder="Email"
+                name="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label
+                className="beer-card__label-text !text-primary"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="form-input__create-account"
+                type="password"
+                placeholder="Password"
+                name="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            {loginError && (
+              <div className="error ml-3 text-xs">{loginError}</div>
             )}
-            Google
-          </button>
-        </div>
-        <div className="divider my-6">
-          <h5 className="my-3">OR</h5>
-        </div>
-        <form>
-          <div className="flex flex-col">
-            <label
-              className="beer-card__label-text !text-primary"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="form-input__create-account"
-              type="email"
-              placeholder="Email"
-              name="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="flex flex-col">
-            <label
-              className="beer-card__label-text !text-primary"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="form-input__create-account"
-              type="password"
-              placeholder="Password"
-              name="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          {loginError && <div className="error ml-3 text-xs">{loginError}</div>}
-        </form>
-        <SaveButton
-          isLoading={isCreateLoading}
-          title="Log in"
-          className="create-btn !bg-accent !text-primary my-6 w-full "
-          onClick={(e) => onSignIn(e, "credentials")}
-        />
-        <div className="text-sm inline-flex gap-2 justify-center w-full">
-          <p className="m-0 ">Don&rsquo;t have an account?</p>
+          </form>
+          <SaveButton
+            isLoading={isCreateLoading}
+            title="Log in"
+            className="create-btn !bg-accent !text-primary my-6 w-full "
+            onClick={(e) => onSignIn(e, "credentials")}
+          />
+          <div className="text-sm inline-flex gap-2 justify-center w-full">
+            <p className="m-0 ">Don&rsquo;t have an account?</p>
 
-          <span className="link link-accent">
-            <Link
-              href={`/auth/create/account${
-                acceptInviteUrl ? `?next=${acceptInviteUrl}` : ""
-              }`}
-            >
-              Sign up here
-            </Link>
-          </span>
+            <span className="link link-accent">
+              <Link
+                href={`/auth/create/account${
+                  acceptInviteUrl ? `?next=${acceptInviteUrl}` : ""
+                }`}
+              >
+                Sign up here
+              </Link>
+            </span>
+          </div>
         </div>
       </div>
     </div>
