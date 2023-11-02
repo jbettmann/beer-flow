1bb8b5305fc302b765deeab033fc8aa9ccaa6ea4
jbettmann <jordan.bettmann@gmail.com>
2023-11-02 11:52:56 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
-import getSingleBeer from "@/lib/getSingleBeer";
-import { signIn, useSession } from "next-auth/react";
-import Link from "next/link";
-import { redirect } from "next/navigation";
-import { useEffect, useState } from "react";
-import { Beer } from "./types/beer";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
-import homepageImage from "../assets/img/homepage.png";
-import mobilePhoneImage from "../assets/img/mobile.png";
-import Image from "next/image";
 import GoogleSignInButton from "@/components/Buttons/GoogleSignInButton";
 import { Mail } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import homepageImage from "../assets/img/homepage.png";
+import mobilePhoneImage from "../assets/img/mobile.png";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
   // let savedBreweryId;
   // if (typeof window !== "undefined") {
   //   savedBreweryId = localStorage.getItem("selectedBreweryId");
