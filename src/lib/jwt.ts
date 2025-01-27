22a09de5041d1f5b3dd8e092118a0c233b8498e0
jbettmann <96999730+jbettmann@users.noreply.github.com>
2025-01-27 16:41:16 -0700

    moved to jose from crypto

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,30 +1,27 @@
-import jwt, { JwtPayload } from "jsonwebtoken";
+import { SignJWT } from "jose";
 
-interface SignOption {
-  expiresIn?: string | number;
-}
+export async function signJwtAccessToken(payload: any, secret: string) {
+  const encoder = new TextEncoder();
+  const secretKey = encoder.encode(secret);
 
-const DEFAULT_SIGN_OPTION: SignOption = {
-  expiresIn: "23h",
-};
-
-export function signJwtAccessToken(
-  payload: JwtPayload,
-  options: SignOption = DEFAULT_SIGN_OPTION
-) {
-  const secret_key = process.env.NEXTAUTH_SECRET;
-  // Incorporating a unique value in the payload
-  payload.iat = Math.floor(Date.now() / 1000); // `iat` stands for "Issued At"
-  const token = jwt.sign(payload, secret_key!, options); // signs with payload, secret_key and options (expiration time).
+  const token = await new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("23h")
+    .sign(secretKey);
 
   return token;
 }
 
-export function signJwtRefreshToken(
-  payload: JwtPayload,
-  options: SignOption = { expiresIn: "30d" }
-) {
-  const secret_key = process.env.REFRESH_TOKEN_SECRET;
-  const token = jwt.sign(payload, secret_key!, options);
+export async function signJwtRefreshToken(payload: any, secret: string) {
+  const encoder = new TextEncoder();
+  const secretKey = encoder.encode(secret);
+
+  const token = await new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("30d")
+    .sign(secretKey);
+
   return token;
 }
