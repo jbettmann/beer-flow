377327096deb1c9e0494f8fe30f07816e748e2f0
jbettmann <96999730+jbettmann@users.noreply.github.com>
2025-02-06 19:05:56 -0700

    updated swr

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -2,12 +2,14 @@
 import { MessagesProvider } from "@/context/messages";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FC, ReactNode } from "react";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, signOut } from "next-auth/react";
 import { BreweryProvider } from "@/context/brewery-beer";
 import { ToastProvider } from "@/context/toast";
 import ThemeProvider from "./layout/ThemeToggle/theme-provider";
 // import { FlatfileProvider } from "@flatfile/react";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { SWRConfig } from "swr";
+import { fetcher } from "@/services/fetcher";
 
 interface ProviderProps {
   children: ReactNode;
@@ -18,21 +20,36 @@ const Provider: FC<ProviderProps> = ({ children }) => {
   const PUBLISHABLE_KEY = "pk_c3e79bef76034e9c94f65581643f09cf";
 
   return (
-    <NuqsAdapter>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <SessionProvider>
-          <BreweryProvider>
-            {/* <FlatfileProvider publishableKey={PUBLISHABLE_KEY}> */}
-            <ToastProvider>
-              <QueryClientProvider client={queryClient}>
-                <MessagesProvider>{children}</MessagesProvider>
-              </QueryClientProvider>
-            </ToastProvider>
-            {/* </FlatfileProvider> */}
-          </BreweryProvider>
-        </SessionProvider>
-      </ThemeProvider>
-    </NuqsAdapter>
+    <SessionProvider>
+      <SWRConfig
+        value={{
+          fetcher,
+          onSuccess: (data, key, config) => {
+            console.log("Data fetched successfully:", data);
+          },
+          onError: async (error) => {
+            if (error.message.includes("401")) {
+              console.warn("Session expired, logging out...");
+              await signOut(); // NextAuth handles token cleanup
+            } else {
+              console.error("Error fetching data:", error);
+            }
+          },
+        }}
+      >
+        <NuqsAdapter>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <BreweryProvider>
+              <ToastProvider>
+                <QueryClientProvider client={queryClient}>
+                  <MessagesProvider>{children}</MessagesProvider>
+                </QueryClientProvider>
+              </ToastProvider>
+            </BreweryProvider>
+          </ThemeProvider>
+        </NuqsAdapter>
+      </SWRConfig>
+    </SessionProvider>
   );
 };
 
